a81faed51c61f675e0edb676a3375437cb7c4c2e
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-14 18:07:05 +0300

    Landmarks app added

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Abdulgafur Demirci on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
