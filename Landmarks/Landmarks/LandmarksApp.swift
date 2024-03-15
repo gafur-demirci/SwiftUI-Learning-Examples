6c043e7754a48cfc2fc168dd2380d06e38f4c28e
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-15 16:42:02 +0300

    Modeldata reading in environment

--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct LandmarksApp: App {
-    var body: some Scene {
-        WindowGroup {
-            LandmarkList()
+    @State private var modelData = ModelData()
+
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+                    .environment(modelData)
+            }
         }
-    }
 }
