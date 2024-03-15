594f6f0d390b113a06b8193fa8434ddb97c68490
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-15 16:45:08 +0300

    LandmarkDetail UI moved

--- Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,24 +8,35 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
-    
+
+
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+
+
     var body: some View {
+        
+        @Bindable var modelData = modelData
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
-            
-            
+
+
             CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
-            
-            
+
+
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-                
-                
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -33,17 +44,16 @@ struct LandmarkDetail: View {
                 }
                 .font(.subheadline)
                 .foregroundStyle(.secondary)
-                
-                
+
+
                 Divider()
-                
-                
+
+
                 Text("About \(landmark.name)")
                     .font(.title2)
                 Text(landmark.description)
             }
             .padding()
-
         }
         .navigationTitle(landmark.name)
         .navigationBarTitleDisplayMode(.inline)
@@ -51,5 +61,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: landmarks[1])
+    let modelData = ModelData()
+        return LandmarkDetail(landmark: modelData.landmarks[1])
+            .environment(modelData)
 }
