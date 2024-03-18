8a01662eaec412cc24ba8589ef1b234dfe50a8b9
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-18 17:47:06 +0300

    ProfileHost view added

---
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Abdulgafur Demirci on 18.03.2024.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
