eea8a7ebd93fe48b4473fe60e1ac2378c5c93d89
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-19 14:32:52 +0300

    watchOS and macOS configurations added

--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -11,10 +11,25 @@ import SwiftUI
 struct LandmarksApp: App {
     @State private var modelData = ModelData()
 
-        var body: some Scene {
-            WindowGroup {
-                ContentView()
-                    .environment(modelData)
-            }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(modelData)
         }
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+        
+        #if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
+        #endif
+    }
 }
