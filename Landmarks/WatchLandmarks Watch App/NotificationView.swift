fdd09b88c792f4dfa5693af250f7661c68406261
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-19 14:39:44 +0300

    WatchLandmark - NotificationView added

---
+++ Landmarks/WatchLandmarks Watch App/NotificationView.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Abdulgafur Demirci on 19.03.2024.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+    
+    var body: some View {
+        VStack {
+            if let landmark {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+            }
+            
+            Text(title ?? "Unknown Landmark")
+                .font(.headline)
+            
+            Divider()
+            
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
+                .font(.caption)
+        }
+    }
+}
+
+#Preview {
+    NotificationView(
+        title: "Turtle Rock",
+        message: "You are within 5 miles of Turtle Rock.",
+        landmark: ModelData().landmarks[0])
+}
