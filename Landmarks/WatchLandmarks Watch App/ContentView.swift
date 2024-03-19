0eede726a86922a5f586b816bc9d4265ffe0fd35
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-19 14:38:54 +0300

    WatchLandmark - ContentView added

---
+++ Landmarks/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Abdulgafur Demirci on 19.03.2024.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
