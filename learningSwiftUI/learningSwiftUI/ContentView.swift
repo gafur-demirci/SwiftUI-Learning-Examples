10b48e0b1b24851f3389bfc227e16ccadff41390
gafur-demirci <gafur.gs.68@gmail.com>
2024-07-15 19:49:49 +0300

    Custom ButtonStyle Struct added

--- learningSwiftUI/learningSwiftUI/ContentView.swift
+++ learningSwiftUI/learningSwiftUI/ContentView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+struct MyStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        let pressed = configuration.isPressed
+        return configuration.label
+            .padding()
+            .border(Color.green, width: 5)
+            .scaleEffect(pressed ? 1.2 : 1.0)
+    }
+}
+
 struct ContentView: View {
     
     @State private var colorActive: Bool = false
