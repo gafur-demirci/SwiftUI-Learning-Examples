741452c4d51bf5c62407fc269368b46100428fa2
gafur-demirci <gafur.gs.68@gmail.com>
2024-07-17 14:07:31 +0300

    Toggle view example added

---
+++ learningSwiftUI/learningSwiftUI/ToggleViewExamples.swift
@@ -0,0 +1,18 @@
+//
+//  ToggleViewExamples.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 17.07.2024.
+//
+
+import SwiftUI
+
+struct ToggleViewExamples: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToggleViewExamples()
+}
