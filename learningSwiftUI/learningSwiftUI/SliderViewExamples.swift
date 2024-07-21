577b14d28000dab291b29662e720c084784f0581
gafur-demirci <gafur.gs.68@gmail.com>
2024-07-21 16:06:30 +0300

    slider view onEditingChanged prop configured

--- learningSwiftUI/learningSwiftUI/SliderViewExamples.swift
+++ learningSwiftUI/learningSwiftUI/SliderViewExamples.swift
@@ -15,7 +15,9 @@ struct SliderViewExamples: View {
     var body: some View {
         VStack{
             Text("Current value: \(currentValue.formatted(.number.precision(.fractionLength(0))))")
-            Slider(value: $currentValue, in: 0...10, step: 1.0)
+                .padding()
+                .background(textActive ? Color.yellow : Color.green)
+            Slider(value: $currentValue, in: 0...10, step: 1.0, onEditingChanged: {self.textActive = $0})
             Spacer()
         }
         .padding()
