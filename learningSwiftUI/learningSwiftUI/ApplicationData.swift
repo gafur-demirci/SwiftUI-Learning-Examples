b121b09fcf3c18289d6fc8b1aedb00789b2f1bfa
gafur-demirci <gafur.gs.68@gmail.com>
2024-07-23 20:29:43 +0300

    Book interface added

--- learningSwiftUI/learningSwiftUI/ApplicationData.swift
+++ learningSwiftUI/learningSwiftUI/ApplicationData.swift
@@ -8,6 +8,22 @@
 import SwiftUI
 import Observation
 
+struct Book: Identifiable, Hashable {
+    let id = UUID()
+    var title: String
+    var author: String
+    var cover: String
+    var year: Int
+    var selected: Bool
+    
+    var displayYear: String {
+        get {
+            return String(year)
+        }
+    }
+}
+
+
 @Observable class ApplicationData {
     var title: String = "Default Title"
     //var titleInput: String = ""
