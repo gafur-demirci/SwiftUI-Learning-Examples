05a3b3f2e0949fe9473b1cf38402480c71a9f096
gafur-demirci <gafur.gs.68@gmail.com>
2024-08-10 01:35:27 +0300

    PickerView simple example added

---
+++ learningSwiftUI/learningSwiftUI/PickerViewExample.swift
@@ -0,0 +1,31 @@
+//
+//  PickerViewExample.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 9.08.2024.
+//
+
+import SwiftUI
+
+struct PickerViewExample: View {
+    
+    @State private var selectedValue: String = "No value"
+    let listCities: [String] = ["Paris","Toronto", "Dublin"]
+    
+    var body: some View {
+        VStack {
+            Text(selectedValue)
+            Picker("Cities", selection: $selectedValue) {
+                ForEach(listCities, id: \.self) { value in
+                    Text(value)
+                }
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PickerViewExample()
+}
