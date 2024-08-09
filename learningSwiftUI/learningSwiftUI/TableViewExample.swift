b6076a125e927400668bde9d05defb32fd41ed9f
gafur-demirci <gafur.gs.68@gmail.com>
2024-08-09 08:30:18 +0300

    TableView select single on tap item and select multiple items using editbutton functionality example added

--- learningSwiftUI/learningSwiftUI/TableViewExample.swift
+++ learningSwiftUI/learningSwiftUI/TableViewExample.swift
@@ -10,22 +10,35 @@ import SwiftUI
 struct TableViewExample: View {
     
     @Environment(ApplicationData.self) private var appData
-    @State private var sort = [KeyPathComparator(\ConsumableItem.name),KeyPathComparator(\ConsumableItem.calories)]
+    @State private var selectedItems: Set<ConsumableItem.ID> = []
+//    @State private var sort = [KeyPathComparator(\ConsumableItem.name),KeyPathComparator(\ConsumableItem.calories)]
     
-    var sortedItems: [ConsumableItem] {
-        let list = appData.listOfItems.sorted(using: sort)
-        return list
-    }
+//    var sortedItems: [ConsumableItem] {
+//        let list = appData.listOfItems.sorted(using: sort)
+//        return list
+//    }
     
     var body: some View {
-        Table(sortedItems, sortOrder: $sort) {
-            TableColumn("Name", value: \.name)
-            TableColumn("Category", value: \.category)
-            TableColumn("Calories", value: \.calories) { item in
-                Text("\(item.calories)")
+        VStack {
+            EditButton()
+            Table(appData.listOfItems, selection: $selectedItems) {
+                TableColumn("Name", value: \.name)
+                TableColumn("Category", value: \.category)
+                TableColumn("Calories") { item in
+                    Text("\(item.calories)")
+                }
+                .width(100)
             }
-            .width(100)
+            Text(listSelected())
+                .padding()
         }
+        
+    }
+    func listSelected() -> String {
+        let list: [String] = selectedItems.map({ id in
+            let item = appData.listOfItems.first(where: { $0.id == id })
+            return item?.name ?? ""})
+        return String(list.sorted().joined(separator: " "))
     }
 }
 
