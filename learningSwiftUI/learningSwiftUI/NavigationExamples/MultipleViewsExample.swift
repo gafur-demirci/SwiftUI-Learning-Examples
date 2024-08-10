c544ee94bd254dacd25e1f9d052dc88931168096
gafur-demirci <gafur.gs.68@gmail.com>
2024-08-10 21:34:25 +0300

    NavigationStack view inside ToolbarItemGroup with an action buttons to do scroll view up and down using ScrollViewReader functionality example added

--- learningSwiftUI/learningSwiftUI/NavigationExamples/MultipleViewsExample.swift
+++ learningSwiftUI/learningSwiftUI/NavigationExamples/MultipleViewsExample.swift
@@ -12,23 +12,43 @@ struct MultipleViewsExample: View {
     @Environment(ApplicationData.self) private var appData
     var body: some View {
         NavigationStack {
-            List(appData.userData) { book in
-                BookView(book: book)
-            }
-            .listStyle(.plain)
-            .navigationTitle(Text("Books"))
-            .navigationBarTitleDisplayMode(.automatic)
-            .toolbar {
-                
-                ToolbarItem(placement: .topBarTrailing, content: {
-                    Button(action: {
-                        print("Delete Item")
-                    }, label: {
-                        Image(systemName: "trash")
+            ScrollViewReader(content: { proxy in
+                List(appData.userData) { book in
+                    BookView(book: book)
+                        .id(book.id)
+                }
+                .listStyle(.plain)
+                .navigationTitle(Text("Books"))
+                .navigationBarTitleDisplayMode(.automatic)
+                .toolbar {
+                    ToolbarItemGroup(placement: .topBarTrailing, content: {
+                        Button(action: {
+                            if let firstIndex = appData.userData.first?.id {
+                                proxy.scrollTo(firstIndex, anchor: .bottom)
+                            }
+                        }, label: {
+                            Image(systemName: "arrow.up.doc")
+                        })
+                        Button(action: {
+                            if let lastIndex = appData.userData.last?.id {
+                                proxy.scrollTo(lastIndex, anchor: .top)
+                            }
+                        }, label: {
+                            Image(systemName: "arrow.down.doc")
+                        })
                     })
-                })
-                
-            }
+                    
+                    /*
+                     ToolbarItem(placement: .topBarTrailing, content: {
+                     Button(action: {
+                     print("Delete Item")
+                     }, label: {
+                     Image(systemName: "trash")
+                     })
+                     })
+                     */
+                }
+            })
         }
     }
 }
