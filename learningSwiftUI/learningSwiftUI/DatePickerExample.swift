08020e869de3e71e9bf468dce69502dd94c5ff3f
gafur-demirci <gafur.gs.68@gmail.com>
2024-08-10 11:24:26 +0300

    MultipleDatePicker view simple usage example added

--- learningSwiftUI/learningSwiftUI/DatePickerExample.swift
+++ learningSwiftUI/learningSwiftUI/DatePickerExample.swift
@@ -9,17 +9,26 @@ import SwiftUI
 
 struct DatePickerExample: View {
     
-    @State private var selectedDate: Date = Date()
+//    @State private var selectedDate: Date = Date()
+    @State private var selectedDates: Set<DateComponents> = []
+    @State private var myDates: String = ""
     
     var body: some View {
         VStack {
-            Text("Date: \(selectedDate.formatted(.dateTime.day().month()))")
-            DatePicker("Date:", selection: $selectedDate, in: Date()..., displayedComponents: .date)
-                .labelsHidden()
-                .datePickerStyle(.wheel)
+            MultiDatePicker("Dates:", selection: $selectedDates)
             Spacer()
+            Text(myDates)
+//            Text("Date: \(selectedDate.formatted(.dateTime.day().month()))")
+//            DatePicker("Date:", selection: $selectedDate, in: Date()..., displayedComponents: .date)
+//                .labelsHidden()
+//                .datePickerStyle(.wheel)
+//            Spacer()
         }
         .padding()
+        .onChange(of: selectedDates, initial: false, { old, values in
+            let days = values.map({ value in String(value.day!)})
+            myDates = days.joined(separator: ",")
+        })
     }
 }
 
