67a1ce766c1515a864c4e5c0744ee50393d405e3
gafur-demirci <gafur.gs.68@gmail.com>
2024-09-01 14:18:00 +0300

    Popover view example added

---
+++ learningSwiftUI/learningSwiftUI/ModalViewExamples/PopoverView.swift
@@ -0,0 +1,30 @@
+//
+//  PopoverView.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 1.09.2024.
+//
+
+import SwiftUI
+
+struct PopoverView: View {
+    
+    @State private var showPopover: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button("Show Popover") {
+                showPopover = true
+            }
+            .popover(isPresented: $showPopover, arrowEdge: .top, content: {
+                HelpView()
+            })
+            Spacer()
+        }
+        .font(.title)
+    }
+}
+
+#Preview {
+    PopoverView()
+}
