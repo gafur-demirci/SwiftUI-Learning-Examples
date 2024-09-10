5b64e529be9b21396095af388523107929259d1c
gafur-demirci <gafur.gs.68@gmail.com>
2024-09-10 13:25:03 +0300

    GeometryReader view example usage on Image view added

--- learningSwiftUI/learningSwiftUI/ViewExamples/GeometryReaderViewExample.swift
+++ learningSwiftUI/learningSwiftUI/ViewExamples/GeometryReaderViewExample.swift
@@ -10,15 +10,19 @@ import SwiftUI
 struct GeometryReaderViewExample: View {
     var body: some View {
         GeometryReader { geometry in
-            let isPortrait = geometry.size.height > geometry.size.width
-            let message = isPortrait ? "Portrait" : "Landscape"
+//            let isPortrait = geometry.size.height > geometry.size.width
+//            let message = isPortrait ? "Portrait" : "Landscape"
             
             HStack {
-                Text(message)
+                Image(.book1)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geometry.size.width / 2, height: geometry.size.height / 4)
+                    .background(Color.gray)
             }
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
         }
-        .ignoresSafeArea()        
+        .ignoresSafeArea()
     }
 }
 
