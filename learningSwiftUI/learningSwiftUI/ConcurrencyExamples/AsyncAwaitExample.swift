33de435f2d813c6eebe8343abb72617ff874df26
gafur-demirci <gafur.gs.68@gmail.com>
2024-09-11 23:06:33 +0300

    Task by explicitly usage example added

--- learningSwiftUI/learningSwiftUI/ConcurrencyExamples/AsyncAwaitExample.swift
+++ learningSwiftUI/learningSwiftUI/ConcurrencyExamples/AsyncAwaitExample.swift
@@ -13,12 +13,16 @@ struct AsyncAwaitExample: View {
             Text("Hello, World!")
                 .padding()
         }
-        .task(priority: .background, {
-            let imageName1 = await loadImage(name: "book1")
-            let imageName2 = await loadImage(name: "book2")
-            let imageName3 = await loadImage(name: "book3")
-            print("\(imageName1), \(imageName2) and \(imageName3)")
-        })
+        // task explicitly
+        .onAppear {
+            Task(priority: .background) {
+                // nine second
+                let imageName1 = await loadImage(name: "book1")
+                let imageName2 = await loadImage(name: "book2")
+                let imageName3 = await loadImage(name: "book3")
+                print("\(imageName1), \(imageName2) and \(imageName3)")
+            }
+        }
     }
     
     func loadImage(name: String) async -> String {
