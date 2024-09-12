c9a6669e8deee1b58c736bcc4eaa33bd5f39ecc2
gafur-demirci <gafur.gs.68@gmail.com>
2024-09-12 23:07:54 +0300

    Custom errors handle simple usage example added

--- learningSwiftUI/learningSwiftUI/ConcurrencyExamples/AsyncAwaitExample.swift
+++ learningSwiftUI/learningSwiftUI/ConcurrencyExamples/AsyncAwaitExample.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+enum MyErrors: Error {
+    case noData, noImage
+}
+
 struct AsyncAwaitExample: View {
     
     var thumbnail: String {
@@ -25,29 +29,42 @@ struct AsyncAwaitExample: View {
         // task explicitly
         .onAppear {
             Task(priority: .background) {
-                // nine second
-                let imageName = await thumbnail
-                print(imageName)
-                //                let imageName2 = await loadImage(name: "book2")
-                //                let imageName3 = await loadImage(name: "book3")
-                //                print("\(imageName1), \(imageName2) and \(imageName3)")
+                do {
+                    // nine second
+                    let imageName = try await loadImage(name: "book10")
+                    print(imageName)
+                    //                let imageName2 = await loadImage(name: "book2")
+                    //                let imageName3 = await loadImage(name: "book3")
+                    //                print("\(imageName1), \(imageName2) and \(imageName3)")
+                } catch MyErrors.noData {
+                    print("Error: No Data Available")
+                } catch MyErrors.noImage {
+                    print("Error: No Image Available")
+                }
             }
             /*
-            Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { (timer) in
-                print("The time is up")
-                myTask.cancel()
-            }
+             Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { (timer) in
+             print("The time is up")
+             myTask.cancel()
+             }
              */
         }
     }
     
-    func loadImage(name: String) async -> String {
-        let result = Task(priority: .background) { () -> String in
-            let imageData = await getMetadata()
-            return "Name: \(name) Size: \(imageData)"
+    func loadImage(name: String) async throws-> String {
+        try? await Task.sleep(nanoseconds: 3 * 1000000000)
+        
+        let error = true
+        if error {
+            throw MyErrors.noImage
         }
-        let message = await result.value
-        return message
+        
+//        let result = Task(priority: .background) { () -> String in
+//            let imageData = await getMetadata()
+//            return "Name: \(name) Size: \(imageData)"
+//        }
+//        let message = await result.value
+        return "Name: \(name)"
         /*
          try? await Task.sleep(nanoseconds: 3 * 1000000000)
          if !Task.isCancelled {
