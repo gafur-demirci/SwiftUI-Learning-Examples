ce8cd53267cfb84fbeff41e5e069dc1ac490a567
gafur-demirci <gafur.gs.68@gmail.com>
2024-10-10 16:11:20 +0300

    AsyncImage view simple usage example added

---
+++ learningSwiftUI/learningSwiftUI/ConcurrencyExamples/AsyncImagesExample.swift
@@ -0,0 +1,24 @@
+//
+//  AsyncImagesExample.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 7.10.2024.
+//
+
+import SwiftUI
+
+struct AsyncImagesExample: View {
+    
+    let website = URL(string: "https://www.formasterminds.com/images/coveruikit4big.png")
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: website)
+        }.padding()
+
+    }
+}
+
+#Preview {
+    AsyncImagesExample()
+}
