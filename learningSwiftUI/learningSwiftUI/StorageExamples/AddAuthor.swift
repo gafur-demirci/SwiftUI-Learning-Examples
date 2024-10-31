6e99a41869c7b651020a8ea4e501a5ce8273fd34
gafur-demirci <gafur.gs.68@gmail.com>
2024-10-31 22:09:14 +0300

    Indention added

--- learningSwiftUI/learningSwiftUI/StorageExamples/AddAuthor.swift
+++ learningSwiftUI/learningSwiftUI/StorageExamples/AddAuthor.swift
@@ -10,37 +10,38 @@ import SwiftData
 
 struct AddAuthor: View {
     @Environment(ApplicationData.self) private var appData
-       @Environment(\.modelContext) var dbContext
-       @State private var nameInput: String = ""
-
-       var body: some View {
-          VStack(alignment: .leading, spacing: 10) {
-             TextField("Insert Name", text: $nameInput)
+    @Environment(\.modelContext) var dbContext
+    @State private var nameInput: String = ""
+    @State private var openAlert: Bool = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            TextField("Insert Name", text: $nameInput)
                 .textFieldStyle(.roundedBorder)
-             HStack {
+            HStack {
                 Spacer()
                 Button("Save") {
-                   storeAuthor()
+                    storeAuthor()
                 }.buttonStyle(.borderedProminent)
-             }
-             Spacer()
-          }.padding()
-       }
-       func storeAuthor() {
-          let name = nameInput.trimmingCharacters(in: .whitespaces)
-          if !name.isEmpty {
-             let newAuthor = Author(name: name, books: [])
-             dbContext.insert(newAuthor)
-             appData.selectedAuthor = newAuthor
-             appData.viewPath.removeLast(2)
-          }
-       }
+            }
+            Spacer()
+        }.padding()
+    }
+    func storeAuthor() {
+        let name = nameInput.trimmingCharacters(in: .whitespaces)
+        if !name.isEmpty {
+            let newAuthor = Author(name: name, books: [])
+            dbContext.insert(newAuthor)
+            appData.selectedAuthor = newAuthor
+            appData.viewPath.removeLast(2)
+        }
+    }
 }
 
 #Preview {
     NavigationStack {
-       AddAuthor()
-          .environment(ApplicationData())
-          .modelContainer(for: [MineBook.self, Author.self], inMemory: true)
+        AddAuthor()
+            .environment(ApplicationData())
+            .modelContainer(for: [MineBook.self, Author.self], inMemory: true)
     }
 }
--- learningSwiftUI/learningSwiftUI/StorageExamples/AddBook.swift
+++ learningSwiftUI/learningSwiftUI/StorageExamples/AddBook.swift
@@ -13,42 +13,42 @@ struct AddBook: View {
     @Environment(\.modelContext) var dbContext
     @State private var titleInput: String = ""
     @State private var yearInput: String = ""
-
+    
     var body: some View {
-       VStack(alignment: .leading, spacing: 10) {
-          TextField("Insert Title", text: $titleInput)
-             .textFieldStyle(.roundedBorder)
-          TextField("Insert Year", text: $yearInput)
-             .textFieldStyle(.roundedBorder)
-             .keyboardType(.numbersAndPunctuation)
-          VStack(alignment: .leading) {
-             Text("Author")
-             NavigationLink(value: "List Authors", label: {
-                Text(appData.selectedAuthor?.name ?? "Select")
-             })
-          }
-          HStack {
-             Spacer()
-             Button("Save") {
-                storeBook()
-             }.buttonStyle(.borderedProminent)
-          }
-          Spacer()
-       }.padding()
+        VStack(alignment: .leading, spacing: 10) {
+            TextField("Insert Title", text: $titleInput)
+                .textFieldStyle(.roundedBorder)
+            TextField("Insert Year", text: $yearInput)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numbersAndPunctuation)
+            VStack(alignment: .leading) {
+                Text("Author")
+                NavigationLink(value: "List Authors", label: {
+                    Text(appData.selectedAuthor?.name ?? "Select")
+                })
+            }
+            HStack {
+                Spacer()
+                Button("Save") {
+                    storeBook()
+                }.buttonStyle(.borderedProminent)
+            }
+            Spacer()
+        }.padding()
     }
     func storeBook() {
-       let title = titleInput.trimmingCharacters(in: .whitespaces)
-       if let year = Int(yearInput), !title.isEmpty {
-          let newBook = MineBook(title: title, author: appData.selectedAuthor, cover: "nocover", year: year)
-          dbContext.insert(newBook)
-          appData.selectedAuthor = nil
-          appData.viewPath.removeLast()
-       }
+        let title = titleInput.trimmingCharacters(in: .whitespaces)
+        if let year = Int(yearInput), !title.isEmpty {
+            let newBook = MineBook(title: title, author: appData.selectedAuthor, cover: "nocover", year: year)
+            dbContext.insert(newBook)
+            appData.selectedAuthor = nil
+            appData.viewPath.removeLast()
+        }
     }
 }
 
 #Preview {
     AddBook()
-       .environment(ApplicationData())
-       .modelContainer(for: [MineBook.self], inMemory: true)
+        .environment(ApplicationData())
+        .modelContainer(for: [MineBook.self], inMemory: true)
 }
