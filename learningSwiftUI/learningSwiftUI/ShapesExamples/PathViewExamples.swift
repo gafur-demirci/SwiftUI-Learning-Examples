5c7eac11c8d5251f14d487463f0e9ba289271065
gafur-demirci <gafur.gs.68@gmail.com>
2024-11-24 19:38:40 +0300

    Curve simple usage added

--- learningSwiftUI/learningSwiftUI/ShapesExamples/PathViewExamples.swift
+++ learningSwiftUI/learningSwiftUI/ShapesExamples/PathViewExamples.swift
@@ -32,6 +32,13 @@ struct PathViewExamples: View {
                 path.addEllipse(in: CGRect(x: 200, y: 140, width: 20, height: 20))
             }
             .stroke(Color.green, style: StrokeStyle(lineWidth: 5))
+            Path { path in
+                path.move(to: CGPoint(x: 50, y: 50))
+                path.addQuadCurve(to: CGPoint(x: 50, y: 200), control: CGPoint(x: 100, y: 125))
+                path.move(to: CGPoint(x: 250, y: 50))
+                path.addCurve(to: CGPoint(x: 250, y: 200), control1: CGPoint(x: 200, y: 125), control2: CGPoint(x: 300, y: 125))
+            }
+            .stroke(Color.yellow, style: StrokeStyle(lineWidth: 5))
         }
     }
 }
