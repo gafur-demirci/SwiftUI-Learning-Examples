5008c3303e88074e58bb1983b2cbb7a8f2f78372
gafur-demirci <gafur.gs.68@gmail.com>
2024-11-24 19:30:39 +0300

    arc Shape example added

--- learningSwiftUI/learningSwiftUI/ShapesExamples/PathViewExamples.swift
+++ learningSwiftUI/learningSwiftUI/ShapesExamples/PathViewExamples.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct PathViewExamples: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 100, y: 150))      // start point
-            path.addLine(to: CGPoint(x: 200 , y: 150))  // move pencel x to 50 point ( start point to line end point )
-            path.addLine(to: CGPoint(x: 100, y: 250))   // move pencel y to 100 point( start previous line end point to line end point )
-            path.closeSubpath()                         // close added lines first line start point end last line end point
+        VStack {
+            Path { path in
+                path.move(to: CGPoint(x: 100, y: 150))      // start point
+                path.addLine(to: CGPoint(x: 200 , y: 150))  // move pencel x to 50 point ( start point to line end point )
+                path.addLine(to: CGPoint(x: 100, y: 250))   // move pencel y to 100 point( start previous line end point to line end point )
+                path.closeSubpath()                         // close added lines first line start point end last line end point
+            }
+            .stroke(Color.blue, style: StrokeStyle(lineWidth: 5))
+            .padding()
+            Path { path in
+                path.move(to: CGPoint(x: 100, y: 150))
+                path.addLine(to: CGPoint(x: 200 , y: 150))
+                path.addArc(center: CGPoint(x: 200, y: 170), radius: 20, startAngle: .degrees(270), endAngle: .degrees(90), clockwise: false)
+                path.addLine(to: CGPoint(x: 100, y: 190))
+                path.closeSubpath()
+            }
+            .stroke(Color.red, style: StrokeStyle(lineWidth: 5))
         }
-        .stroke(Color.blue, style: StrokeStyle(lineWidth: 5))
     }
 }
 
