e383078f96c7e99eb2ce6dcda0acecdc0ee3c4e9
gafur-demirci <gafur.gs.68@gmail.com>
2024-11-24 15:48:33 +0300

    Gradient Stop usage example added

--- learningSwiftUI/learningSwiftUI/ShapesExamples/CommonShapes.swift
+++ learningSwiftUI/learningSwiftUI/ShapesExamples/CommonShapes.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct CommonShapes: View {
     
     let lineStyle = StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round, miterLimit: 0, dash: [20], dashPhase: 0)
-    let gradient: Gradient = Gradient(colors: [.red, .green])
+    let gradient: Gradient = Gradient(stops: [
+        Gradient.Stop(color: .red, location: 0.0),
+        Gradient.Stop(color: .green, location: 0.4)
+    ])
     @State private var setActive: Bool = true
     
     var body: some View {
