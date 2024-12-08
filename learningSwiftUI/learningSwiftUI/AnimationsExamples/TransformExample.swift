7bf7082017eec95d5ffa33540c6527e9e1e06283
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-08 10:50:33 +0300

    Rotation on shape view example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/TransformExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/TransformExample.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct TransformExample: View {
     var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .rotation(.degrees(45))
+            .fill(Color.red)
+            .frame(width: 100, height: 100)
+        /*
         Image("book5")
             .resizable()
             .scaledToFit()
             .frame(width: 150, height: 200)
             .clipShape(Circle())
+         */
     }
 }
 
