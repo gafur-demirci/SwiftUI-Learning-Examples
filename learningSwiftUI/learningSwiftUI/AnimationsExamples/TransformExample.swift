cb3718697b28e79a22a25b77fb884f0fa9934bda
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-08 10:55:00 +0300

    trim modifier example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/TransformExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/TransformExample.swift
@@ -11,12 +11,18 @@ struct TransformExample: View {
     var body: some View {
         HStack {
             Triangle()
+                .trim(from: 0, to: 0.70)
+                .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round))
+                .frame(width: 100, height: 100)
+            /*
+            Triangle()
                 .fill(Color.blue)
                 .frame(width: 100, height: 100)
             Triangle()
                 .scale(x: -1, y: 1)
                 .fill(Color.blue)
                 .frame(width: 100, height: 100)
+             */
         }
         /*
         RoundedRectangle(cornerRadius: 20)
