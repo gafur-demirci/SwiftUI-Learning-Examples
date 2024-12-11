ea3ea44f01f8106acf80ec164d4b9cc5601b4ac3
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-11 20:52:49 +0300

    clip simple usage example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct CanvasExample: View {
     var body: some View {
         Canvas { context, size in
-            let imageReady = context.resolve(Image("book1"))
             let imageFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
             context.translateBy(x: size.width/2, y: size.height/2)
+            
+            let clipFrame = CGRect(x: -100, y: -100, width: 200, height: 200)
+            context.clip(to: Circle().path(in: clipFrame))
+            
             for _ in 0..<10 {
                 context.rotate(by: .degrees(36))
-                context.draw(imageReady, in: imageFrame)
+                context.draw(Image("book1"), in: imageFrame)
             }
         }
         .ignoresSafeArea()
