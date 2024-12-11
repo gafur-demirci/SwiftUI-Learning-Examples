0919dd2a9ece87503e1a25bb88828b086b6438a0
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-11 20:39:18 +0300

    Different type draw canvas example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct CanvasExample: View {
     var body: some View {
         Canvas { context, size in
-            let imageSize = CGSize(width: 161, height: 216)
-            let posX = (size.width - imageSize.width) / 2
-            let posY = posX
-            
-            let imageFrame = CGRect(x: posX, y: posY, width: imageSize.width, height: imageSize.height)
+            let imageFrame = CGRect(x: 60, y: 75, width: 215, height: 288)
             context.draw(Image("book3"), in: imageFrame)
+            
+            let circleFrame = CGRect(x: 20, y: 50, width: 60, height: 60)
+            context.fill(Circle().path(in: circleFrame), with: .color(.yellow))
+            
+            let rectFrame = CGRect(x: 50, y: 60, width: 250, height: 40)
+            context.fill(RoundedRectangle(cornerRadius: 25).path(in: rectFrame), with: .color(.yellow))
+            
+            let textPos = CGPoint(x: 80, y: 80)
+            context.draw(Text("My Picture").font(.title.bold()), at: textPos, anchor: .leading)
         }
         .ignoresSafeArea()
     }
