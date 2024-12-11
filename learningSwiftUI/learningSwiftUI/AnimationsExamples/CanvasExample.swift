c929130e65c1cf61798aab5b1c067b5ee9368c66
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-11 20:58:50 +0300

    copy original context and draw different context positioning view example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
@@ -10,16 +10,18 @@ import SwiftUI
 struct CanvasExample: View {
     var body: some View {
         Canvas { context, size in
-            let imageFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
-            context.translateBy(x: size.width/2, y: size.height/2)
+            var copyContext = context
+            copyContext.translateBy(x: size.width/2, y: size.height/2)
+            copyContext.rotate(by: .degrees(45))
+            let imageFrame = CGRect(x: -85, y: -100, width: 161, height: 216)
+            copyContext.draw(Image("book1"), in: imageFrame)
             
-            let clipFrame = CGRect(x: -100, y: -100, width: 200, height: 200)
-            context.clip(to: Circle().path(in: clipFrame))
+            let center = size.width/2
+            let rectFrame = CGRect(x: center - 125, y: 160, width: 250, height: 40)
+            context.fill(RoundedRectangle(cornerRadius: 25).path(in: rectFrame),with: .color(.yellow))
             
-            for _ in 0..<10 {
-                context.rotate(by: .degrees(36))
-                context.draw(Image("book1"), in: imageFrame)
-            }
+            let textPos = CGPoint(x: center, y: 180)
+            context.draw(Text("My Picture").font(.title.bold()), at: textPos, anchor: .center)
         }
         .ignoresSafeArea()
     }
