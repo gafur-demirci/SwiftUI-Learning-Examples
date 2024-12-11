7e0bbeca32ae4c23ffe51b6b7b5a6dbe503d1622
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-11 21:01:22 +0300

    image filter simple usage example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
@@ -10,18 +10,12 @@ import SwiftUI
 struct CanvasExample: View {
     var body: some View {
         Canvas { context, size in
-            var copyContext = context
-            copyContext.translateBy(x: size.width/2, y: size.height/2)
-            copyContext.rotate(by: .degrees(45))
-            let imageFrame = CGRect(x: -85, y: -100, width: 161, height: 216)
-            copyContext.draw(Image("book1"), in: imageFrame)
+            context.addFilter(.colorMultiply(.red))
+            context.addFilter(.blur(radius: 5))
             
-            let center = size.width/2
-            let rectFrame = CGRect(x: center - 125, y: 160, width: 250, height: 40)
-            context.fill(RoundedRectangle(cornerRadius: 25).path(in: rectFrame),with: .color(.yellow))
-            
-            let textPos = CGPoint(x: center, y: 180)
-            context.draw(Text("My Picture").font(.title.bold()), at: textPos, anchor: .center)
+            let margin = (size.width - 161) / 2
+            let imageFrame = CGRect(x: margin, y: margin, width: 161, height: 216)
+            context.draw(Image("book1"), in: imageFrame)
         }
         .ignoresSafeArea()
     }
