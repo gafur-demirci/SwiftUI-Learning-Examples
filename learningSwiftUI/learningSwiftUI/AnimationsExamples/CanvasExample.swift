7c6b9c8fc47a75f3101a9921c8abd1f8f0efa25e
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-11 20:41:33 +0300

    Rotate and draw rotated image example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/CanvasExample.swift
@@ -10,17 +10,10 @@ import SwiftUI
 struct CanvasExample: View {
     var body: some View {
         Canvas { context, size in
-            let imageFrame = CGRect(x: 60, y: 75, width: 215, height: 288)
+            let imageFrame = CGRect(x: 60, y: 75, width: 161, height: 216)
             context.draw(Image("book3"), in: imageFrame)
-            
-            let circleFrame = CGRect(x: 20, y: 50, width: 60, height: 60)
-            context.fill(Circle().path(in: circleFrame), with: .color(.yellow))
-            
-            let rectFrame = CGRect(x: 50, y: 60, width: 250, height: 40)
-            context.fill(RoundedRectangle(cornerRadius: 25).path(in: rectFrame), with: .color(.yellow))
-            
-            let textPos = CGPoint(x: 80, y: 80)
-            context.draw(Text("My Picture").font(.title.bold()), at: textPos, anchor: .leading)
+            context.rotate(by: .degrees(20))
+            context.draw(Image("book3"), in: imageFrame)
         }
         .ignoresSafeArea()
     }
