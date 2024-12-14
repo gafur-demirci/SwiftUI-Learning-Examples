8c01614176d75238509449fd5a03b9ba1efef1ac
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-14 15:37:30 +0300

    Chart view modifiers for chartXSelection simple usage example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/ChartExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/ChartExample.swift
@@ -10,31 +10,19 @@ import Charts
 
 struct ChartExample: View {
     @Environment(ChartData.self) private var chartData
+    @State private var selectedFood: String? = nil
     
     var body: some View {
         VStack {
-            Chart {
-                ForEach(chartData.listOfItems) { item in
-                    BarMark(x: .value("Name", item.name), y: .value("Calories", item.calories))
-                        .foregroundStyle(.cyan)
-                        .opacity(0.5)
-                        .cornerRadius(20)
-                }
-                RuleMark(y: .value("Average", averageCalories()))
-                    .foregroundStyle(.black)
-                    .lineStyle(StrokeStyle(lineWidth: 5))
-                    .annotation(position: .top, alignment: .leading) {
-                        Text("Average Calories")
-                    }
+            Chart(chartData.listOfItems) { item in
+                BarMark(x: .value("Name", item.name), y: .value("Calories", item.calories))
+                    .foregroundStyle(item.name == selectedFood ? .yellow : .blue)
             }
             .frame(height: 300)
             .padding()
+            .chartXSelection(value: $selectedFood)
             Spacer()
         }
-    }
-    func averageCalories() -> Int {
-        let total = chartData.listOfItems.reduce(0, { $0 + $1.calories })
-        return total / chartData.listOfItems.count
     }
 }
                          
