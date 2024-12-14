dd255a5f2714ea2476603aa4a83d6194561b9b06
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-14 21:55:29 +0300

    ImageRenderer simple usage example added

---
+++ learningSwiftUI/learningSwiftUI/GraphicsExamples/ImageRendererExample.swift
@@ -0,0 +1,46 @@
+//
+//  ImageRendererExample.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 14.12.2024.
+//
+
+import SwiftUI
+
+struct ImageRendererExample: View {
+    
+    @State private var pattern: UIImage?
+    let newPicture: NewPictureView = NewPictureView()
+    
+    var body: some View {
+        VStack {
+            newPicture
+            Button("Export Image") {
+                let renderer = ImageRenderer(content: newPicture)
+                if let img = renderer.uiImage {
+                    pattern = img.preparingThumbnail(of: CGSize(width: 55.5, height: 83))
+                }
+            }
+            if let pattern {
+                Rectangle()
+                    .fill(.image(Image(uiImage: pattern)))
+                    .frame(width: 222, height: 332)
+            }
+            Spacer()
+        }
+    }
+}
+
+struct NewPictureView: View {
+    var body: some View {
+        Image("book10")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 111, height: 166)
+            .clipShape(Circle())
+    }
+}
+
+#Preview {
+    ImageRendererExample()
+}
