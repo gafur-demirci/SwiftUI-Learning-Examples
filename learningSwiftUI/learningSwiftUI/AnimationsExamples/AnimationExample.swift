78491f2e16a84f91c9bdec2eee81f9131572228a
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-15 12:51:58 +0300

    View apply animation simple usage example added

---
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/AnimationExample.swift
@@ -0,0 +1,39 @@
+//
+//  AnimationExample.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 15.12.2024.
+//
+
+import SwiftUI
+
+struct AnimationExample: View {
+    
+    @State private var boxScale: CGFloat = 1
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 50, height: 50)
+                    .scaleEffect(boxScale)
+                Triangle()
+                    .fill(Color.blue)
+                    .frame(width: 50, height: 50)
+                    .scaleEffect(boxScale)
+            }
+            .frame(width: 250, height: 120)
+            Button("Animate") {
+                withAnimation(.linear) {
+                    boxScale = boxScale == 1 ? 2 : 1
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AnimationExample()
+}
