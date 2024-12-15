2f66b39b3a32e3c8eaa231a7b1775b854efe49e7
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-15 15:36:37 +0300

    Add and Remove view with transition using default animation example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/AnimationExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/AnimationExample.swift
@@ -7,45 +7,24 @@
 
 import SwiftUI
 
-class animationViewData {
-    var radius: CGFloat = 0
-    var step: CGFloat = 5
-    var lastTime: Double = 0
-    var maxTime: Double = 0.02
-}
-
 struct AnimationExample: View {
     
-    let viewData = animationViewData()
+    @State private var showInfo: Bool = false
     
     var body: some View {
-        TimelineView(.animation) { time in
-            let interval = time.date.timeIntervalSinceReferenceDate
-            let delta = interval - viewData.lastTime
-            
-            Canvas { context, size in
-                if delta > viewData.maxTime {
-                    calculateRadius()
-                    viewData.lastTime = interval
-                }
-                let rad = viewData.radius
-                let circleFrame = CGRect(x: size.width / 2 - rad, y: size.height / 2 - rad, width: rad * 2, height: rad * 2)
-                context.fill(Circle().path(in: circleFrame), with: .color(.red))
+        VStack {
+            Button("Show Information") {
+                showInfo.toggle()
             }
-            .ignoresSafeArea()
+            .padding()
+            if showInfo {
+                Text("This is the information")
+                    .transition(.scale.animation(.default))
+            }
+            Spacer()
         }
     }
-    func calculateRadius() {
-        viewData.radius = viewData.radius + viewData.step
-        if viewData.step < 0 && viewData.radius < 0 {
-            viewData.radius = 0
-            viewData.step = 5
-        }
-        if viewData.step > 0 && viewData.radius > 150 {
-            viewData.radius = 150
-            viewData.step = -5
-        }
-    }
+
 }
 
 #Preview {
