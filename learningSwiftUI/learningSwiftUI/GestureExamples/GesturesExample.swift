2c0b8d51423226d2481586251746d58829b6ba69
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-15 20:45:01 +0300

    allowHitTesting simple usage example added on controlled Toggle view binding state property

--- learningSwiftUI/learningSwiftUI/GestureExamples/GesturesExample.swift
+++ learningSwiftUI/learningSwiftUI/GestureExamples/GesturesExample.swift
@@ -10,24 +10,22 @@ import SwiftUI
 struct GesturesExample: View {
     
     @State private var expanded: Bool = false
-    @State private var pressing: Bool = false
+    @State private var allowExpansion: Bool = false
     
     var body: some View {
         Image("book1")
             .resizable()
             .scaledToFit()
             .frame(width: 160, height: 200)
-            .opacity(pressing ? 0 : 1)
-            .onLongPressGesture(minimumDuration: 1, maximumDistance: 10, perform: {
+            .onTapGesture {
                 expanded = true
-            }, onPressingChanged: { value in
-                withAnimation(.easeInOut(duration: 1.5)) {
-                    pressing = value
-                }
-            })
+            }
+            .allowsHitTesting(allowExpansion)
             .sheet(isPresented: $expanded) {
                 ShowImage()
             }
+        Toggle("", isOn: $allowExpansion)
+            .labelsHidden()
     }
 }
 
