80917d98bf60430f4b3c301e04c8c12a888b4960
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-15 20:22:25 +0300

    onLongPressGesture simple usage example added

--- learningSwiftUI/learningSwiftUI/GestureExamples/GesturesExample.swift
+++ learningSwiftUI/learningSwiftUI/GestureExamples/GesturesExample.swift
@@ -10,15 +10,21 @@ import SwiftUI
 struct GesturesExample: View {
     
     @State private var expanded: Bool = false
+    @State private var pressing: Bool = false
     
     var body: some View {
         Image("book1")
             .resizable()
             .scaledToFit()
             .frame(width: 160, height: 200)
-            .onTapGesture {
+            .opacity(pressing ? 0 : 1)
+            .onLongPressGesture(minimumDuration: 1, maximumDistance: 10, perform: {
                 expanded = true
-            }
+            }, onPressingChanged: { value in
+                withAnimation(.easeInOut(duration: 1.5)) {
+                    pressing = value
+                }
+            })
             .sheet(isPresented: $expanded) {
                 ShowImage()
             }
