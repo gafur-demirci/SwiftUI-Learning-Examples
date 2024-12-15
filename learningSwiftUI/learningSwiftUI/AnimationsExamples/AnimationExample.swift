8e41d8001a2e937ee333184902b366e9936f5323
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-15 15:08:12 +0300

    Animation Canvas inside TimelineView for using custom class properties example added

--- learningSwiftUI/learningSwiftUI/AnimationsExamples/AnimationExample.swift
+++ learningSwiftUI/learningSwiftUI/AnimationsExamples/AnimationExample.swift
@@ -7,16 +7,32 @@
 
 import SwiftUI
 
+class animationViewData {
+    var posX: CGFloat = 0
+    var posY: CGFloat = 0
+    var lastTime: Double = 0
+    var maxTime: Double = 0.5
+}
+
 struct AnimationExample: View {
     
+    let viewData = animationViewData()
+    
     var body: some View {
-        TimelineView(.periodic(from: Date(), by: 3)) { time in
-            let calendar = Calendar.current
-            let components = calendar.dateComponents([.second], from: time.date)
-            HStack {
-                Text("Time: \(components.second ?? 0)")
-                    .font(.largeTitle.bold())
+        TimelineView(.animation) { time in
+            let interval = time.date.timeIntervalSinceReferenceDate
+            let delta = interval - viewData.lastTime
+            
+            Canvas { context, size in
+                if delta > viewData.maxTime {
+                    viewData.posX = CGFloat.random(in: 0..<size.width - 20)
+                    viewData.posY = CGFloat.random(in: 0..<size.height - 20)
+                    viewData.lastTime = interval
+                }
+                let circleFrame = CGRect(x: viewData.posX, y: viewData.posY, width: 20, height: 20)
+                context.fill(Circle().path(in: circleFrame), with: .color(.red))
             }
+            .ignoresSafeArea()
         }
     }
 }
