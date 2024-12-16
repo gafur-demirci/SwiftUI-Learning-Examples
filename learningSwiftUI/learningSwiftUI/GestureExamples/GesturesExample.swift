c87704422961cd9b5187b1f488839b41d3acbfb1
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-16 17:23:43 +0300

    dropDestination modifier modifier for drag and dropping screen added opacity example added

--- learningSwiftUI/learningSwiftUI/GestureExamples/GesturesExample.swift
+++ learningSwiftUI/learningSwiftUI/GestureExamples/GesturesExample.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct GesturesExample: View {
     
     @State private var picture: Image = Image("nocover")
+    @State private var didEnter: Bool = false
     
     var body: some View {
         
@@ -18,12 +19,15 @@ struct GesturesExample: View {
             .scaledToFit()
             .frame(minWidth: 0, maxWidth: .infinity)
             .frame(height: 400)
+            .overlay(didEnter ? Color.green.opacity(0.2) : Color.clear)
             .dropDestination(for: Image.self, action: { elements, location in
                 if let image = elements.first {
                     picture = image
                     return true
                 }
                 return false
+            }, isTargeted: { value in
+                didEnter = value
             })
         Spacer()
     }
