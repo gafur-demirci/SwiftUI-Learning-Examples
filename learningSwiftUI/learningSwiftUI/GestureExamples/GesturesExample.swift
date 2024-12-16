bc3d1565b87afbbd4fe809b30487b5e40214c83f
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-16 17:21:09 +0300

    dropDestination modifier simple usage example added

--- learningSwiftUI/learningSwiftUI/GestureExamples/GesturesExample.swift
+++ learningSwiftUI/learningSwiftUI/GestureExamples/GesturesExample.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct GesturesExample: View {
     
+    @State private var picture: Image = Image("nocover")
+    
     var body: some View {
         
-        Image("book1")
+        picture
             .resizable()
             .scaledToFit()
-            .frame(width: 300, height: 400)
-            .draggable(Image("book1"), preview: {
-                Image(systemName: "scope")
-                    .font(.system(size: 50))
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .frame(height: 400)
+            .dropDestination(for: Image.self, action: { elements, location in
+                if let image = elements.first {
+                    picture = image
+                    return true
+                }
+                return false
             })
         Spacer()
     }
