6e5352d3ea31496b707d05d3f049178816f08b16
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-19 18:39:48 +0300

    AddBookInNoti view simple codes added

---
+++ learningSwiftUI/learningSwiftUI/NotificationExamples/AddBookInNoti.swift
@@ -0,0 +1,51 @@
+//
+//  AddBookInNoti.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 19.12.2024.
+//
+
+import SwiftUI
+
+struct AddBookInNoti: View {
+    
+    @EnvironmentObject var notificationData: NotificationData
+    @Environment(\.dismiss) var dismiss
+    @State private var titleInput: String = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Title")
+                TextField("Insert Title", text: $titleInput)
+                    .textFieldStyle(.roundedBorder)
+            }
+            HStack {
+                Spacer()
+                Button("Save") {
+                    let title = titleInput.trimmingCharacters(in: .whitespaces)
+                    if !title.isEmpty {
+                        addValue(title: title)
+                        dismiss()
+                    }
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Add Book")
+    }
+    
+    func addValue(title: String) {
+        notificationData.titles.append(title)
+        
+        let center = NotificationCenter.default
+        let namme = Notification.Name("Update Data")
+        center.post(name: namme, object: nil, userInfo: nil)
+    }
+}
+
+#Preview {
+    AddBookInNoti()
+        .environmentObject(NotificationData())
+}
---
+++ learningSwiftUI/learningSwiftUI/NotificationExamples/NotificationData.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationData.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 19.12.2024.
+//
+
+import SwiftUI
+
+struct NotificationData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationData()
+}
