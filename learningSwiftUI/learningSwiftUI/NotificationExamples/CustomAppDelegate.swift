a7f5dfe45e646e757f798c07680841b5fb1343ad
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-21 20:13:14 +0300

    Custom scene delegate simple usage example added

--- learningSwiftUI/learningSwiftUI/NotificationExamples/CustomAppDelegate.swift
+++ learningSwiftUI/learningSwiftUI/NotificationExamples/CustomAppDelegate.swift
@@ -12,4 +12,11 @@ class CustomAppDelegate: NSObject, UIApplicationDelegate {
         print("App has launched")
         return true
     }
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "Custom Delegate", sessionRole: connectingSceneSession.role)
+        if connectingSceneSession.role == .windowApplication {
+            config.delegateClass = CustomSceneDelegate.self
+        }
+        return config
+    }
 }
---
+++ learningSwiftUI/learningSwiftUI/NotificationExamples/CustomSceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  CustomSceneDelegate.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 21.12.2024.
+//
+
+import UIKit
+
+class CustomSceneDelegate: NSObject, UIWindowSceneDelegate {
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+       print("Scene was created")
+    }
+}
