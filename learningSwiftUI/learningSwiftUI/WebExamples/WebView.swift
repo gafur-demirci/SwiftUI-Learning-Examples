8d804ae3a408fc5ec6ab7073e1552a969a866368
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-23 11:46:39 +0300

    Customized WKWebView simple usge example added

--- learningSwiftUI/learningSwiftUI/WebExamples/WebView.swift
+++ learningSwiftUI/learningSwiftUI/WebExamples/WebView.swift
@@ -9,14 +9,46 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    let url: URL
+    @Binding var url: String
+    let view: WKWebView = WKWebView()
     
     func makeUIView(context: Context) -> WKWebView {
-        let view = WKWebView()
-        let request = URLRequest(url: url)
+        view.navigationDelegate = context.coordinator
+        let request = URLRequest(url: URL(string: "https://google.com")!)
         view.load(request)
         return view
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {}
+    
+    func loadWeb(web: String) {
+        var components = URLComponents(string: web)
+        components?.scheme = "https"
+        if let newUrl = components?.string {
+            if let url = newUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                if let loadUrl = URL(string: url) {
+                    let request = URLRequest(url: loadUrl)
+                    view.load(request)
+                }
+            }
+        }
+    }
+    
+    func makeCoordinator() -> CoordinatorWebView {
+        return CoordinatorWebView(input: $url)
+    }
+}
+
+class CoordinatorWebView: NSObject, WKNavigationDelegate {
+    @Binding var input: String
+    
+    init(input: Binding<String>) {
+        self._input = input
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if let webURL = webView.url {
+            input = webURL.absoluteString
+        }
+    }
 }
--- learningSwiftUI/learningSwiftUI/WebExamples/WebViewExample.swift
+++ learningSwiftUI/learningSwiftUI/WebExamples/WebViewExample.swift
@@ -7,14 +7,39 @@
 
 import SwiftUI
 
+class WebViewData: ObservableObject {
+    @Published var url: String = ""
+}
+
 struct WebViewExample: View {
+    @ObservedObject var webViewData: WebViewData = WebViewData()
+    var webview: WebView!
     
-    @State private var searchURL: URL = URL(string: "https://www.softtech.com.tr")!
+    init() {
+        webview = WebView(url: $webViewData.url)
+    }
+    
+//    @State private var searchURL: URL = URL(string: "https://www.softtech.com.tr")!
 //    @State private var openSheet: Bool = false
 //    @State private var disableButton: Bool = false
     
     var body: some View {
-        WebView(url: searchURL)
+        VStack {
+            HStack {
+                TextField("Insert URL", text: $webViewData.url)
+                    .textInputAutocapitalization(.none)
+                    .autocorrectionDisabled(true)
+                Button("Load") {
+                    let text = webViewData.url.trimmingCharacters(in: .whitespaces)
+                    if !text.isEmpty {
+                        webview.loadWeb(web: text)
+                    }
+                }
+            }
+            .padding(5)
+            webview
+        }
+//        WebView(url: searchURL)
         /*
         VStack {
             Button("Open Browser") {
