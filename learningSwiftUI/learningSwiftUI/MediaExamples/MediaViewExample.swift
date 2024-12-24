040b7d15b60db8fd056ec3d621d7c6d09653f340
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-24 16:53:30 +0300

    ImagePicker view with taked picture saved in to photos library simple usage example added

--- learningSwiftUI/learningSwiftUI/MediaExamples/MediaViewExample.swift
+++ learningSwiftUI/learningSwiftUI/MediaExamples/MediaViewExample.swift
@@ -7,28 +7,57 @@
 
 import SwiftUI
 
+class ImageData: NSObject, ObservableObject {
+    @Published var path: NavigationPath = NavigationPath()
+    @Published var picture: UIImage?
+}
+
 struct MediaViewExample: View {
     
-    @State private var path = NavigationPath()
-    @State private var picture: UIImage?
-
+    @ObservedObject var imageData: ImageData = ImageData()
+    @State private var showAlert: Bool = false
+    var ImagePickerView: ImagePicker!
+    
+    init() {
+        ImagePickerView = ImagePicker(path: $imageData.path, picture: $imageData.picture)
+    }
+    
     var body: some View {
-       NavigationStack(path: $path) {
-          VStack {
-             HStack {
+        NavigationStack(path: $imageData.path) {
+            VStack {
+                HStack {
+                    Button("Share Picture"){
+                        showAlert = true
+                    }
+                    .disabled(imageData.picture == nil ? true : false)
+                    Spacer()
+                    NavigationLink("Get Picture", value: "Open Picker")
+                }
+                .navigationDestination(for: String.self, destination: { _ in
+                    ImagePickerView
+                })
+                .alert("Save Picture", isPresented: $showAlert, actions: {
+                    Button("Cancel", role: .cancel, action: {
+                        showAlert = false
+                    })
+                    Button("Yes", role: .none, action: {
+                        if let picture = imageData.picture {
+                            UIImageWriteToSavedPhotosAlbum(picture, nil, nil, nil)
+                        }
+                    })
+                }, message: {
+                    Text("Do you want to store the picture in the Photos Library?")
+                })
+                Image(uiImage: imageData.picture ?? UIImage(named: "nocover")!)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .clipped()
                 Spacer()
-                NavigationLink("Get Picture", value: "Open Picker")
-             }.navigationDestination(for: String.self, destination: { _ in
-                ImagePicker(path: $path, picture: $picture)
-             })
-             Image(uiImage: picture ?? UIImage(named: "nocover")!)
-                .resizable()
-                .scaledToFill()
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                .clipped()
-             Spacer()
-          }.padding()
-       }.statusBarHidden()
+            }
+            .padding()
+        }
+        .statusBarHidden()
     }
 }
 
