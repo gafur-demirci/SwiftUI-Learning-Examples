b00f07dee381c0215251f6b6905bfe4959e7ea45
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-24 20:26:06 +0300

    Custom video player with controls for play and pause example added

--- learningSwiftUI/learningSwiftUI/MediaExamples/CustomPlayerData.swift
+++ learningSwiftUI/learningSwiftUI/MediaExamples/CustomPlayerData.swift
@@ -15,16 +15,19 @@ class PlayerViewData: NSObject {
     @Published var playerObservation: NSKeyValueObservation?
     @Published var playerLayer: AVPlayerLayer?
     
-    func setObserver() {
-        playerObservation = playerItem?.observe(\.status, options: [.new], changeHandler: { item, value in
-                if item.status == .readyToPlay {
-                    self.player?.play()
-            }
-        })
-    }
+    //    func setObserver() {
+    //        playerObservation = playerItem?.observe(\.status, options: [.new], changeHandler: { item, value in
+    //                if item.status == .readyToPlay {
+    //                    self.player?.play()
+    //            }
+    //        })
+    //    }
 }
 
 @Observable class CustomPlayerData {
+    
+    var playing: Bool = false
+    var progress: CGFloat = 0
     
     @ObservationIgnored var customVideoView: PlayerView!
     @ObservationIgnored var viewData: PlayerViewData
@@ -35,13 +38,32 @@ class PlayerViewData: NSObject {
         
         let bundle = Bundle.main
         let url = bundle.url(forResource: "videotrees", withExtension: "mp4")
-        let asset = AVURLAsset(url: url!)
         
+        let asset = AVURLAsset(url: url!)
         viewData.playerItem = AVPlayerItem(asset: asset)
         viewData.player = AVPlayer(playerItem: viewData.playerItem)
         
         viewData.playerLayer = customVideoView.view.layer as? AVPlayerLayer
         viewData.playerLayer?.player = viewData.player
-        viewData.setObserver()        
+        
+        let interval = CMTime(value: 1, timescale: 2)
+        viewData.player?.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main, using: { time in
+            if let duration = self.viewData.playerItem?.duration {
+                let position = time.seconds / duration.seconds
+                self.progress = CGFloat(position)
+            }
+        })
+    }
+    
+    func playVideo() {
+        if viewData.playerItem?.status == .readyToPlay {
+            if playing {
+                viewData.player?.pause()
+                playing = false
+            } else {
+                viewData.player?.play()
+                playing = true
+            }
+        }
     }
 }
--- learningSwiftUI/learningSwiftUI/MediaExamples/CustomVideoView.swift
+++ learningSwiftUI/learningSwiftUI/MediaExamples/CustomVideoView.swift
@@ -12,8 +12,32 @@ struct CustomVideoView: View {
     @Environment(CustomPlayerData.self ) private var playerData
     
     var body: some View {
-        playerData.customVideoView
-            .ignoresSafeArea()
+        ZStack {
+            playerData.customVideoView
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                HStack {
+                    Button(playerData.playing ? "Pause" : "Play") {
+                        playerData.playVideo()
+                    }
+                    .frame(width: 70)
+                    .foregroundStyle(.black)
+                    GeometryReader { geometry in
+                        HStack {
+                            Rectangle()
+                                .fill(Color(red: 0, green: 0.4, blue: 0.8, opacity: 0.8))
+                                .frame(width: geometry.size.width * playerData.progress, height: 20)
+                            Spacer()
+                        }
+                    }
+                    .padding(.top, 15)
+                }
+                .padding([.leading, .trailing])
+                .frame(height: 50)
+                .background(Color(red: 0.9, green: 0.9, blue: 0.9, opacity: 0.8))
+            }
+        }
     }
 }
 
