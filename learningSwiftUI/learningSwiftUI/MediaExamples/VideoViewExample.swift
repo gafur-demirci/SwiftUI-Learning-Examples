c6b555a8e327b9e0bbec0b072429247207c7902a
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-24 19:46:02 +0300

    VideoPlayer to videoOverlay configuration example added

--- learningSwiftUI/learningSwiftUI/MediaExamples/VideoViewExample.swift
+++ learningSwiftUI/learningSwiftUI/MediaExamples/VideoViewExample.swift
@@ -14,7 +14,19 @@ struct VideoViewExample: View {
     
     var body: some View {
         if videoData.player != nil {
-            VideoPlayer(player: videoData.player!)
+            VideoPlayer(player: videoData.player!, videoOverlay: {
+                VStack {
+                    Text("Title: Trees at the park")
+                        .font(.title)
+                        .padding([.top, .bottom], 8)
+                        .padding([.leading, .trailing], 16)
+                        .foregroundStyle(.black)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(10)
+                        .padding(.top, 8)
+                    Spacer()
+                }
+            })
                 .onAppear {
                     videoData.player?.play()
                 }
