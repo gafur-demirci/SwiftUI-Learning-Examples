0ed3ab9b4517e984395c7c1b69e51bd8ea06de61
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-25 19:46:40 +0300

    Custom struct for using Transferable protocol for importableFromServices modifiers by add the view new image from other device example added

--- learningSwiftUI/learningSwiftUI/MultiplatformExamples/MultiplatformView.swift
+++ learningSwiftUI/learningSwiftUI/MultiplatformExamples/MultiplatformView.swift
@@ -7,20 +7,42 @@
 
 import SwiftUI
 
+#if os(macOS)
+struct ImageRepresentation: Transferable {
+    let image: NSImage
+    
+    static var transferRepresentation: some TransferRepresentation {
+        DataRepresentation(importedContentType: .jpeg, importing: { data in
+            if let newImage = NSImage(data: data) {
+                return ImageRepresentation(image: newImage)
+            } else {
+                return ImageRepresentation(image: NSImage(named: "nocover")!)
+            }
+        })
+    }
+}
+#endif
+
 struct MultiplatformView: View {
     
-    @State private var visibility: NavigationSplitViewVisibility = .automatic
-    /** Bir file'ın sadece belli platformlarda build olması isteniyorsa Build Phases'ta Compile Sources içerisinde ilgili file için düzenleme yapılmalıdır.*/
+    @State private var MyPicture = Image("nocover")
+    
     var body: some View {
-        NavigationSplitView(columnVisibility: $visibility, sidebar: {
-           MenuView()
-        }, detail: {
-           #if os(macOS)
-              MacDetailView()
-           #else
-              MobileDetailView()
-           #endif
+        VStack {
+            MyPicture
+                .resizable()
+                .scaledToFit()
+        }
+        .frame(width: 500, height: 350)
+        #if os(macOS)
+        .importableFromServices(for: ImageRepresentation.self, action: {elements in
+            if let value = elements.first {
+                MyPicture = Image(nsImage: value.image)
+                return true
+            }
+            return false
         })
+        #endif
     }
 }
 
