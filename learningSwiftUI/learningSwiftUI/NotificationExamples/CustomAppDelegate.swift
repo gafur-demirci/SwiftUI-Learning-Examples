511f568931beef007e4ec6c7e4d208a912485d0f
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-25 14:13:03 +0300

    Multiplatfom build fix

--- learningSwiftUI/learningSwiftUI/NotificationExamples/CustomAppDelegate.swift
+++ learningSwiftUI/learningSwiftUI/NotificationExamples/CustomAppDelegate.swift
@@ -4,8 +4,9 @@
 //
 //  Created by Abdulgafur Demirci on 21.12.2024.
 //
-
+#if os(iOS)
 import UIKit
+
 
 class CustomAppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -22,3 +23,4 @@ class CustomAppDelegate: NSObject, UIApplicationDelegate {
         return config
     }
 }
+#endif
