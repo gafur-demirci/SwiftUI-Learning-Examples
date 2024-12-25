41f961c86a52f07df0be20ea6a3b2d92d8a365cd
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-25 19:30:02 +0300

    Replace to New Item option in file menu to empty addition example added

--- learningSwiftUI/learningSwiftUI/learningSwiftUIApp.swift
+++ learningSwiftUI/learningSwiftUI/learningSwiftUIApp.swift
@@ -71,6 +71,7 @@ struct learningSwiftUIApp: App {
         }
         #if os(macOS)
         .commands {
+            CommandGroup(replacing: .newItem, addition: { })
             CommandGroup(after: .newItem, addition: {
                 Button("Option 1") {
                     print("This is option 1")
