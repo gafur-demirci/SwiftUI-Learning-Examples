b26fbdf6b4bcbc6300a146ac05bd0b08393ce55f
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-29 15:24:14 +0300

    TabView inside the custom tab for role configuration simple usage example added

---
+++ learningSwiftUI/learningSwiftUI/ModalViewExamples/TabSearchView.swift
@@ -0,0 +1,33 @@
+//
+//  TabSearchView.swift
+//  learningSwiftUI
+//
+//  Created by Abdulgafur Demirci on 29.12.2024.
+//
+
+import SwiftUI
+
+@available(iOS 18.0, *)
+struct TabSearchView: View {
+    var body: some View {
+        TabView {
+            Tab("Home", systemImage: "book.circle") {
+                Text("Main Screen")
+            }
+            Tab("Settings", systemImage: "gear") {
+                Text("Settings")
+            }
+            Tab(role: .search) {
+                Text("Search")
+            }
+        }
+    }
+}
+
+#Preview {
+    if #available(iOS 18.0, *) {
+        TabSearchView()
+    } else {
+        // Fallback on earlier versions
+    }
+}
