61bd4de904a6c372e73383aa6ca6f52e78c681ac
gafur-demirci <gafur.gs.68@gmail.com>
2024-12-30 19:23:25 +0300

    Commentted codes removed

--- learningSwiftUI/learningSwiftUI/ViewExamples/ButtonExamples.swift
+++ learningSwiftUI/learningSwiftUI/ViewExamples/ButtonExamples.swift
@@ -26,43 +26,6 @@ struct ButtonExamples: View {
     
     var body: some View {
         VStack {
-//            HeaderView(title: title, titleInput: $titleInput)
-//            Text("Default Title")
-//                .padding()
-//                .frame(minWidth: 0, maxWidth: expanded ? .infinity : 150, maxHeight: 50)
-//                .background(Color.yellow)
-//                .foregroundStyle(colorActive ? Color.black : Color.blue)
-//            Button(action: {
-//                expanded.toggle()
-//            }, label: {
-//                VStack {
-//                    Image(expanded ? "contract" : "expand")
-//                        .renderingMode(.template)
-//                    Text(expanded ? "Contract" : "Expand")
-//                }
-//            })
-//            .disabled(buttonDisabled)
-//            HStack {
-//                Button("Delete", role: .destructive) {
-//                    print("Delete Action...")
-//                }.buttonStyle(.bordered)
-//                Spacer()
-//                Button("Send") {
-//                    print("Send Information")
-//                }.buttonStyle(.borderedProminent)
-//            }
-//            Button(action: {
-//                print("Delete item")
-//            }, label: {
-//                HStack {
-//                    Image(systemName: "mail")
-//                        .imageScale(.large)
-//                    Text("Send")
-//                }
-//            })
-//            .buttonStyle(.borderedProminent)
-//            .font(.largeTitle)
-//            .controlSize(.large)
             Text("Default Title")
                 .padding()
                 .foregroundStyle(color)
@@ -79,21 +42,6 @@ struct ButtonExamples: View {
         colorActive.toggle()
     }
 }
-
-//struct HeaderView: View {
-//    
-//    var title: String
-//    @Binding var titleInput: String
-//    
-//    var body: some View {
-//        VStack{
-//            Text(title)
-//                .padding(10)
-//            TextField("Insert Title", text: $titleInput)
-//                .textFieldStyle(.roundedBorder)
-//        }
-//    }
-//}
 
 #Preview {
     ButtonExamples()
