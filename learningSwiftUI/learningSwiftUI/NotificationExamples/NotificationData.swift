a481a49ed0e14bb8cef0f2c3eb91c4f23b25b139
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-03 20:15:13 +0300

    Commentted codes removed

--- learningSwiftUI/learningSwiftUI/NotificationExamples/NotificationData.swift
+++ learningSwiftUI/learningSwiftUI/NotificationExamples/NotificationData.swift
@@ -39,44 +39,5 @@ import SwiftUI
 //    let center = NotificationCenter.default
 //    var scrollOffset: CGFloat = 0
 //    var isLandscape: Bool = false
-    /*
-    init() {
-        Task(priority: .background, operation: {
-            await receiveNotificationOpen()
-        })
-        Task(priority: .background, operation: {
-            await receiveNotificationClose()
-        })
-        Task(priority: .background, operation: {
-            await receiveNotification()
-        })
-    }
-    
-    @MainActor
-    func receiveNotification() async {
-        let name = UIDevice.orientationDidChangeNotification
-        for await _ in center.notifications(named: name, object: nil) {
-            let device = UIDevice.current
-            let orientation = device.orientation
-            isLandscape = orientation.isLandscape
-        }
-    }
-    
-    @MainActor
-    func receiveNotificationOpen() async {
-        let name = UIWindow.keyboardDidShowNotification
-        for await _ in center.notifications(named: name, object: nil) {
-            scrollOffset = 20
-        }
-    }
-    
-    @MainActor
-    func receiveNotificationClose() async {
-        let name = UIWindow.keyboardDidHideNotification
-        for await _ in center.notifications(named: name, object: nil) {
-            scrollOffset = 0
-        }
-    }
-    */
 }
 #endif
