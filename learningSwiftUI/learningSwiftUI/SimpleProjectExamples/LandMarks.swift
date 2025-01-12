25ecd0b4dadc0e0610aad0dcb8f9c3f1c10ebf00
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-12 17:10:15 +0300

    LandMarkData updated

--- learningSwiftUI/learningSwiftUI/SimpleProjectExamples/LandMarks.swift
+++ learningSwiftUI/learningSwiftUI/SimpleProjectExamples/LandMarks.swift
@@ -8,25 +8,43 @@
 import SwiftUI
 
 struct LandMarks: View {
-    @EnvironmentObject private var landMarkData: LandMarkData
-
+    @Environment(LandMarkData.self) private var landMarkData: LandMarkData
+    
+    @State private var selectedLandmark = Set<Landmark.ID>()
+    
     var body: some View {
-        List {
-            ForEach(landMarkData.landmarks) { landmark in
-                VStack(alignment: .leading) {
-                    Text(landmark.name)
-                        .font(.headline)
-                        .padding(.bottom, 5)
-//                    Image(uiImage: UIImage(data: landmark.picture) ?? UIImage(named: "nocover")!)
-//                        .resizable()
-//                        .scaledToFit()
+//        Table(of: Landmark.self, columns: {
+//            TableColumn("Name", value: \.name)
+//        }, rows: {
+//            ForEach(landMarkData.landmarks) { landmark in
+//                TableRow(landmark)
+//            }
+//        })
+        Table(landMarkData.landmarks, selection: $selectedLandmark) {
+                    TableColumn("Name", value: \.name)
+
                 }
-            }
+        Text("\(selectedLandmark)")
+        Button("Mark") {
+            print(selectedLandmark)
         }
+//        List {
+//            ForEach(landMarkData.landmarks) { landmark in
+//                VStack(alignment: .leading) {
+//                    Text(landmark.name)
+//                        .font(.headline)
+//                        .padding(.bottom, 5)
+//                }
+//            }
+//        }
     }
+    
+//    func tableRowSelection(_ selection: Set<Landmark.ID>) {
+//        selectedLandmark = selection
+//    }
 }
 
 #Preview {
     LandMarks()
-        .environmentObject(LandMarkData())
+        .environment(LandMarkData())
 }
