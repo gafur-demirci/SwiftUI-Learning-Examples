81d1996f6f50eef3c907230bb1ce00861800a8b2
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-12 18:50:53 +0300

    LandMarkData added place option property and configured

--- learningSwiftUI/learningSwiftUI/SimpleProjectExamples/LandMarkData.swift
+++ learningSwiftUI/learningSwiftUI/SimpleProjectExamples/LandMarkData.swift
@@ -8,23 +8,25 @@
 import SwiftUI
 import Observation
 
-struct Landmark: Identifiable {
+struct Landmark: Identifiable, Hashable {
     var id = UUID()
     var name: String
     var picture: String
+    var place: String?
 }
 @Observable class LandMarkData {
     
     var landmarks: [Landmark] = []
     
     init() {
-        landmarks.append(Landmark(name: "London", picture: "london"))
-        landmarks.append(Landmark(name: "Paris", picture: "paris"))
-        landmarks.append(Landmark(name: "Tokyo", picture: "tokyo"))
-        landmarks.append(Landmark(name: "New York", picture: "newyork"))
-        landmarks.append(Landmark(name: "Berlin", picture: "berlin"))
-        landmarks.append(Landmark(name: "Madrid", picture: "madrid"))
-        landmarks.append(Landmark(name: "Rome", picture: "rome"))
-        landmarks.append(Landmark(name: "Amsterdam", picture: "amsterdam"))
+        landmarks.append(Landmark(name: "London", picture: "london", place: "London Eye"))
+        landmarks.append(Landmark(name: "Paris", picture: "paris", place: "Eifel Tower"))
+        landmarks.append(Landmark(name: "Tokyo", picture: "tokyo", place: "Meiji Tapestry"))
+        landmarks.append(Landmark(name: "New York", picture: "newyork", place: "Freedom Tower"))
+        landmarks.append(Landmark(name: "Berlin", picture: "berlin", place: "Brandenburg Door"))
+        landmarks.append(Landmark(name: "Madrid", picture: "madrid", place: "Retiro Park"))
+        landmarks.append(Landmark(name: "Rome", picture: "rome", place: "Colosseum"))
+        landmarks.append(Landmark(name: "Amsterdam", picture: "amsterdam", place: "Van Gogh Museum"))
+
     }
 }
