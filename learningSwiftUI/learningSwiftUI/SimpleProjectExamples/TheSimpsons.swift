847857ad345139b34c173b4578200cfb49d7b416
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-13 16:11:11 +0300

    TheSimpsons view using Table view configuration added

--- learningSwiftUI/learningSwiftUI/SimpleProjectExamples/TheSimpsons.swift
+++ learningSwiftUI/learningSwiftUI/SimpleProjectExamples/TheSimpsons.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct TheSimpsons: View {
+    
+    @Environment(SimpsonData.self) private var simpsonData: SimpsonData
+    @State private var selectedSimpson = Set<Simpson.ID>()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Table(simpsonData.simpsons, selection: $selectedSimpson) {
+                TableColumn("Name") { simpson in
+                    NavigationLink(destination: TheSimpson(simpson: simpson)) {
+                        Text(simpson.name)
+                            .font(.headline)
+                            .foregroundStyle(.black)
+                            .padding(.bottom, 5)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Simpsons")
     }
 }
 
 #Preview {
     TheSimpsons()
+        .environment(SimpsonData())
 }
