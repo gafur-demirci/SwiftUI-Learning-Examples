f7fa3b47eaa323c44e3ed86791594a19757f50d9
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-14 09:34:41 +0300

    ArtBoook main view for listing item added

--- learningSwiftUI/learningSwiftUI/SimpleProjectExamples/ArtBook.swift
+++ learningSwiftUI/learningSwiftUI/SimpleProjectExamples/ArtBook.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ArtBook: View {
+    
+    @Environment(SimpsonData.self) private var simpsonData: SimpsonData
+    @State private var selectedSimpson = Set<Simpson.ID>()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Table(simpsonData.simpsons, selection: $selectedSimpson) {
+                TableColumn("Name") { simpson in
+                    NavigationLink(destination: TheSimpson(simpson: simpson)) {
+                        Text(simpson.name)
+                            .font(.headline)
+                            .foregroundStyle(.black)
+                            .padding(.bottom, 5)
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Simpsons")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(value: "Settings View", label: {
+                        Image(systemName: "gear")
+                    })
+                }
+            }
+            .navigationDestination(for: String.self, destination: { viewId in
+                if viewId == "Settings View" {
+                    SettingsView()
+                }
+            })
+        }
     }
 }
 
 #Preview {
     ArtBook()
+        .environment(SimpsonData())
 }
