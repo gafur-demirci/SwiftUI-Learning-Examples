3a79272f79bc14783060a1d906843605dde74609
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-16 20:21:12 +0300

    ArtData model file configured

--- learningSwiftUI/learningSwiftUI/SimpleProjectExamples/ArtData.swift
+++ learningSwiftUI/learningSwiftUI/SimpleProjectExamples/ArtData.swift
@@ -5,4 +5,26 @@
 //  Created by Abdulgafur Demirci on 16.01.2025.
 //
 
-import Foundation
+import SwiftUI
+import Observation
+
+class Art : Identifiable {
+    var id = UUID()
+    var name: String
+    var artist: String
+    var year: Int32
+    var image: UIImage
+    
+    init(name: String, artist: String, year: Int32, image: UIImage) {
+        self.name = name
+        self.artist = artist
+        self.year = year
+        self.image = image
+    }
+}
+
+@Observable class ArtData {
+    
+    var arts: [Art] = []
+    
+}
