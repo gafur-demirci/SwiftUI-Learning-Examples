8c7085cce8e5ade6a2ec64abe06b5744cb774178
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-19 18:04:52 +0300

    TravelBook app project initial codes added

---
+++ TravelBookApp/TravelBook/TravelBook/TravelBookApp.swift
@@ -0,0 +1,20 @@
+//
+//  TravelBookApp.swift
+//  TravelBook
+//
+//  Created by Abdulgafur Demirci on 19.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelBookApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
