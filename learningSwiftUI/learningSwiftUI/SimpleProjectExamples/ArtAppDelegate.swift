d2600766fe76f18bb45a59776592585d8c66451a
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-19 15:47:40 +0300

    ArtBook view table items show crash fixed

--- learningSwiftUI/learningSwiftUI/SimpleProjectExamples/ArtAppDelegate.swift
+++ learningSwiftUI/learningSwiftUI/SimpleProjectExamples/ArtAppDelegate.swift
@@ -8,6 +8,34 @@
 import UIKit
 import CoreData
 
+class PersistenceController {
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+    
+    init() {
+        container = NSPersistentContainer(name: "ArtBookModel")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+    var context: NSManagedObjectContext {
+        return container.viewContext
+    }
+    func save() {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
+
 class ArtAppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
