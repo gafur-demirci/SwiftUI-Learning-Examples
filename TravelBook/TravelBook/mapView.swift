79017808e48716b94ce4ef49146636d4bd6e70c4
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-20 21:00:52 +0300

    mapView configured

---
+++ TravelBook/TravelBook/mapView.swift
@@ -0,0 +1,18 @@
+//
+//  mapView.swift
+//  TravelBook
+//
+//  Created by Abdulgafur Demirci on 19.01.2025.
+//
+
+import SwiftUI
+
+struct mapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapView()
+}
