44fbd56ee433d0acc18a02c68ff7416febffd7b8
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-21 19:20:38 +0300

    Some commented codes removed

--- TravelBook/TravelBook/TravelBookApp.swift
+++ TravelBook/TravelBook/TravelBookApp.swift
@@ -13,8 +13,6 @@ struct TravelBookApp: App {
 
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
             mapView()
         }
     }
