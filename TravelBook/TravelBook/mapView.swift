a85d919a791af46e88a793561761561343cbbd66
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-22 20:44:56 +0300

    Check user location access request simple usage example added

--- TravelBook/TravelBook/mapView.swift
+++ TravelBook/TravelBook/mapView.swift
@@ -7,14 +7,34 @@
 
 import SwiftUI
 import MapKit
+import CoreLocationUI
 
 struct mapView: View {
+    
+    @Environment(MapData.self ) private var mapData
+    
     var body: some View {
-        Map()
-            .padding([.top, .bottom], 10)
+        Map(position: Bindable(mapData).cameraPos) {
+            if mapData.isAuthorized {
+                UserAnnotation()
+            }
+        }
+        .mapControls {
+            if mapData.isAuthorized {
+                MapUserLocationButton()
+            }
+        }
+        .safeAreaInset(edge: .bottom, content: {
+
+            LocationButton(.currentLocation) {
+                mapData.cameraPos = .userLocation(fallback: .automatic)
+            }
+            .padding()
+        })
     }
 }
 
 #Preview {
     mapView()
+        .environment(MapData())
 }
