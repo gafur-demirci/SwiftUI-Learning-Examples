53f3c14f767dfc400bee19888d1db32b8cc481e3
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-27 18:36:47 +0300

    AppDelegate added for coreData usage

---
+++ eCommerce/eCommerce/Utilities/AppDelegate.swift
@@ -0,0 +1,78 @@
+//
+//  AppDelegate.swift
+//  eCommerce
+//
+//  Created by Abdulgafur Demirci on 26.01.2025.
+//
+
+import UIKit
+import CoreData
+import SwiftData
+
+class PersistenceController {
+    static let shared = PersistenceController()
+    let container: ModelContainer
+    
+    init() {
+        container = NSPersistentContainer(name: "ArtBookModel")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+    var context: ModelContext {
+        return container.viewContext
+    }
+    func save() {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "Custom Delegate", sessionRole: connectingSceneSession.role)
+        if connectingSceneSession.role == .windowApplication {
+            config.delegateClass = SceneDelegate.self
+        }
+        return config
+    }
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Handle discarded scene sessions.
+    }
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ArtBookModel")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
