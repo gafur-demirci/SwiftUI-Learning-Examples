c98c567bc9d3a771a4e98ab22530e442da1e4da9
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-27 18:35:00 +0300

    Register view added

--- eCommerce/eCommerce/Authentication/Register.swift
+++ eCommerce/eCommerce/Authentication/Register.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct Register: View {
+//    @ObservedObject var userManager: UserManager
+    @State private var username = ""
+    @State private var password = ""
+    @State private var userType: User.UserType = .customer
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Kullanıcı Adı", text: $username)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            SecureField("Şifre", text: $password)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Picker("Kullanıcı Tipi", selection: $userType) {
+                Text("Admin").tag(User.UserType.admin)
+                Text("Müşteri").tag(User.UserType.customer)
+                Text("Satıcı").tag(User.UserType.seller)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            Button("Kayıt Ol") {
+//                userManager.registerUser(username: username, password: password, userType: userType)
+//                let newUser = User()
+//                modelContext.insert(newUser)
+//                do {
+//                    try modelContext.save()
+//                    print("User Saved!")
+//                } catch {
+//                    print("Veriler kaydedilemedi: \(error.localizedDescription)")
+//                }
+            }
+            .padding()
+        }
+        .padding()
     }
 }
 
