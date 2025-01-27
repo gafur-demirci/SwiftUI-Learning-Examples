96936d73a48582d43de74055dae7ce0c8ff67925
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-27 18:36:30 +0300

    UserManager changes but not working

--- eCommerce/eCommerce/User/UserManager.swift
+++ eCommerce/eCommerce/User/UserManager.swift
@@ -7,41 +7,61 @@
 
 import SwiftUI
 import SwiftData
+import UIKit
 
 class UserManager: ObservableObject {
     @Published var user: User?
     var modelContainer: ModelContainer
-    // ModelContext'i tanımlıyoruz
-    var context: ModelContext {
-        modelContainer.viewContext
-    }
-
+    
     init() {
-        // ModelContainer'ı başlatıyoruz
-        self.modelContainer = try! ModelContainer(for: User.self)
-        self.context = modelContainer.viewContext
-        fetchUser()
-    }
-
-    // Kullanıcıyı veritabanından getir
-    func fetchUser() {
-        let context = modelContainer.viewContext
-        if let existingUser = try? context.fetch(FetchDescriptor<User>()).first {
-            self.user = existingUser
-        } else {
-            let newUser = User(username: "Yeni Kullanıcı")
-            self.user = newUser
-            context.insert(newUser)
-            try? context.save()
+        do {
+            self.modelContainer = try ModelContainer(for: User.self)
+            self.user = nil
+//            fetchUser()
+        }
+        catch {
+            // Handle error appropriately, perhaps by logging or showing an alert
+            print("Failed to initialize ModelContainer: \(error)")
+            do {
+                self.modelContainer = try ModelContainer()
+                self.user = nil
+            } catch {
+                print("Failed to initialize ModelContainer: \(error)")
+            }
         }
     }
 
-    // Kullanıcıyı güncelle
-    func updateUser(username: String, profileImage: UIImage?) {
-        if let user = user {
-            user.username = username
-            user.profileImage = profileImage
-            try? modelContainer.viewContext.save()
-        }
-    }
+//    // Fetch user from the database
+//    func fetchUser() {
+//        let context = modelContainer.viewContext
+//        if let existingUser = try? context.fetch(FetchDescriptor<User>()).first {
+//            self.user = existingUser
+//        } else {
+//            let newUser = User(username: "Yeni Kullanıcı")
+//            self.user = newUser
+//            context.insert(newUser)
+//            do {
+//                try context.save()
+//            } catch {
+//                // Handle error appropriately
+//                print("Failed to save context: \(error)")
+//            }
+//        }
+//    }
+//    
+//    // Update user details
+//    func updateUser(username: String, profileImage: UIImage?) {
+//        if let user = user {
+//            user.username = username
+//            user.profileImage = profileImage
+//            do {
+//                try modelContainer.viewContext.save()
+//            } catch {
+//                // Handle error appropriately
+//                print("Failed to save context: \(error)")
+//            }
+//        }
+//    }
+
 }
+
