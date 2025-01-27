292d04da1e4debfc60bbc5376d1c546294f5052a
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-27 18:36:11 +0300

    User model updated

--- eCommerce/eCommerce/User/User.swift
+++ eCommerce/eCommerce/User/User.swift
@@ -9,13 +9,20 @@ import Foundation
 import SwiftData
 import UIKit
 
+enum UserType: String {
+    case admin, customer, seller
+}
+
 @Model
 class User {
     @Attribute(.unique) var id: UUID
     var username: String
     var profileImageData: Data?
+    var password: String  // Şifre yönetimi için
+    var userType: UserType
+    var lastLoginDate: Date?
 
-    init(username: String, profileImageData: Data? = nil) {
+    init(username: String, password: String, userType: UserType, lastLoginDate: Date? = nil, profileImageData: Data? = nil) {
         self.id = UUID()
         self.username = username
         self.profileImageData = profileImageData
@@ -32,16 +39,3 @@ class User {
         }
     }
 }
-
-//@Model
-//class User {
-//    @Attribute(.primaryKey) var id: UUID
-//    @Attribute(.required) var username: String
-//    @Attribute(.optional) var profileImage: UIImage?
-//    
-//    init(username: String, profileImage: UIImage? = nil) {
-//        self.username = username
-//        self.profileImage = profileImage
-//        self.id = UUID()
-//    }
-//}
