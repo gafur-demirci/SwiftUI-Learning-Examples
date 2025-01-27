e6288be912f4225b0442fa0af726965bae800ff2
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-27 18:35:20 +0300

    CartView some changes

--- eCommerce/eCommerce/Cart/CartView.swift
+++ eCommerce/eCommerce/Cart/CartView.swift
@@ -8,21 +8,37 @@
 import SwiftUI
 
 struct CartView: View {
+//    @ObservedObject var userManager: UserManager
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Sepetiniz Boş")
-                    .font(.title)
-                    .foregroundColor(.gray)
-                    .padding()
-
-                Spacer()
-            }
-            .navigationTitle("Sepet")
+        VStack {
+//            if let user = userManager.user {
+//                let cartItems = userManager.fetchCartItems()
+//                
+//                if cartItems.isEmpty {
+                    Text("Sepetiniz boş.")
+//                } else {
+//                    List(cartItems) { cartItem in
+//                        VStack(alignment: .leading) {
+//                            Text("Ürün: \(cartItem.product.name)")
+//                            Text("Fiyat: \(cartItem.product.price) TL")
+//                            Text("Miktar: \(cartItem.quantity)")
+//                        }
+//                    }
+//                }
+//            }
+//        else {
+//                Text("Kullanıcı yükleniyor...")
+//            }
+//            
+//            Spacer()
         }
+        .padding()
+        .navigationTitle("Sepet")
     }
 }
-
-#Preview {
-    CartView()
-}
+/*
+ #Preview {
+ CartView(userManager: UserManager())
+ }
+ */
