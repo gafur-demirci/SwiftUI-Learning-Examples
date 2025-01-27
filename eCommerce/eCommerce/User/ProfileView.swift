47c10e2bd57d790779eea8d13db227b05d5f4101
gafur-demirci <gafur.gs.68@gmail.com>
2025-01-27 18:36:01 +0300

    Profile view stateless

--- eCommerce/eCommerce/User/ProfileView.swift
+++ eCommerce/eCommerce/User/ProfileView.swift
@@ -9,24 +9,24 @@ import SwiftUI
 import SwiftData
 
 struct ProfileView: View {
-//    @State private var username = "Ahmet Yılmaz" // Örnek kullanıcı adı
-//    @State private var profileImage: UIImage? = nil // Profil resmi, nil ise avatar gösterilecek
+    @State private var username = "Ahmet Yılmaz" // Örnek kullanıcı adı
+    @State private var profileImage: UIImage? = nil // Profil resmi, nil ise avatar gösterilecek
 //    @Environment(\.modelContainer) private var modelContainer
     @State private var showEditProfile = false
     @State private var showCallConfirmation = false
     
-    @StateObject private var userManager: UserManager
+//    @StateObject private var userManager: UserManager
 
     // UserManager'ı dışarıdan başlatıyoruz
-    init() {
-        _userManager = StateObject(wrappedValue: UserManager()) // UserManager'ı başlatıyoruz
-    }
+//    init() {
+//        _userManager = StateObject(wrappedValue: UserManager()) // UserManager'ı başlatıyoruz
+//    }
 
     var body: some View {
         NavigationView {
             VStack(spacing: 20) {
                 // Kullanıcı Bilgisi ve Avatar
-                UserProfileHeader(username: userManager.user?.username ?? "", profileImage: userManager.user?.profileImage)
+                UserProfileHeader(username: username, profileImage: profileImage)
                 
                 // Siparişlerim Listesi
                 OrderListSection()
@@ -42,7 +42,7 @@ struct ProfileView: View {
             .padding()
             .navigationTitle("Profil")
             .sheet(isPresented: $showEditProfile) {
-                EditProfile(userManager: userManager)
+//                EditProfile(userManager: userManager)
             }
             .alert(isPresented: $showCallConfirmation) {
                 Alert(
