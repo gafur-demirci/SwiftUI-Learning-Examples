08b074153ca2ebf6abd5b7805f5ac83e8655592d
gafur-demirci <gafur.gs.68@gmail.com>
2025-02-09 16:40:56 +0300

    SignUp view added for sign up user in firebase database

--- instaClone/instaClone/SignUpView.swift
+++ instaClone/instaClone/SignUpView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SignUpView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("Hello, World!")
+        }
+        .toolbarVisibility(.hidden)
+        
     }
 }
 
