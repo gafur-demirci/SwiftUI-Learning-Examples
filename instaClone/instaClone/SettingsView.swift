c4a1c2c8a81bbbd381ac92d1280e658a3af9d921
gafur-demirci <gafur.gs.68@gmail.com>
2025-02-14 21:23:21 +0300

    SignOut functionality added with authentication

--- instaClone/instaClone/SettingsView.swift
+++ instaClone/instaClone/SettingsView.swift
@@ -6,14 +6,28 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct SettingsView: View {
+    
+    @State private var isSignedOut: Bool = false
+    
     var body: some View {
         NavigationStack {
             Text("Settings")
                 .padding()
-            NavigationLink(destination: ContentView()) {
+            Button(action: {
+                do {
+                    try Auth.auth().signOut()
+                    isSignedOut = true
+                } catch {
+                    print("Error while signing out: \(error.localizedDescription)")
+                }
+            }, label: {
                 Text("Çıkış Yap")
+            })
+            .navigationDestination(isPresented: $isSignedOut) {
+                ContentView()
             }
         }
         .toolbarVisibility(.hidden)
