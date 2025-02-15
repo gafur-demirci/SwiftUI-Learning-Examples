1253925814ce7e6c0f09a444930c783fab352b35
gafur-demirci <gafur.gs.68@gmail.com>
2025-02-15 16:20:49 +0300

    ImagePicker controller added for user pick a photo in photo library

--- instaClone/instaClone/util/ImagePicker.swift
+++ instaClone/instaClone/util/ImagePicker.swift
@@ -1,44 +1,46 @@
-import SwiftUI
-#if os(iOS)
-struct ImagePicker: UIViewControllerRepresentable {
-   @Binding var path: NavigationPath
-   @Binding var picture: UIImage?
+//
+//  ImagePicker.swift
+//  instaClone
+//
+//  Created by Abdulgafur Demirci on 15.02.2025.
+//
 
-   func makeUIViewController(context: Context) -> UIImagePickerController {
-      let mediaPicker = UIImagePickerController()
-      mediaPicker.delegate = context.coordinator
-      if UIImagePickerController.isSourceTypeAvailable(.camera) {
-         mediaPicker.sourceType = .camera
-         mediaPicker.mediaTypes = ["public.image"]
-         mediaPicker.allowsEditing = false
-         mediaPicker.cameraCaptureMode = .photo
-      } else {
-         print("The media is not available")
-      }
-      return mediaPicker
-   }
-   func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var picture: UIImage?
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let mediaPicker = UIImagePickerController()
+        mediaPicker.delegate = context.coordinator
+//        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+//            mediaPicker.sourceType = .camera
+//            mediaPicker.mediaTypes = ["public.image"]
+//            mediaPicker.allowsEditing = false
+//            mediaPicker.cameraCaptureMode = .photo
+//        } else {
+//            print("The media is not available")
+//        }
+        return mediaPicker
+    }
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
     
-   func makeCoordinator() -> ImagePickerCoordinator {
-      ImagePickerCoordinator(path: $path, picture: $picture)
-   }
+    func makeCoordinator() -> ImagePickerCoordinator {
+        ImagePickerCoordinator(self)
+    }
 }
 class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-   @Binding var path: NavigationPath
-   @Binding var picture: UIImage?
-
-   init(path: Binding<NavigationPath>, picture: Binding<UIImage?>) {
-      self._path = path
-      self._picture = picture
-   }
-   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-      if let newpicture = info[.originalImage] as? UIImage {
-         picture = newpicture
-      }
-      path = NavigationPath()
-   }
-   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-      path = NavigationPath()
-   }
+    
+    let parent : ImagePicker
+    
+    init(_ parent : ImagePicker) {
+        self.parent = parent
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let newpicture = info[.originalImage] as? UIImage {
+            parent.picture = newpicture
+        }
+        parent.presentationMode.wrappedValue.dismiss()
+    }
 }
-#endif
