91a6a7eef59aa03f0c8cf06136ea28304c38d3f8
gafur-demirci <gafur.gs.68@gmail.com>
2025-02-15 16:21:08 +0300

    Upload feed view basic configuration added

--- instaClone/instaClone/UploadView.swift
+++ instaClone/instaClone/UploadView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct UploadView: View {
+    
+    @State var comments: String = ""
+    @State var image: UIImage?
+    @State var isPresented: Bool = false
+    
     var body: some View {
         Text("Upload")
+        VStack {
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .onTapGesture {
+                        isPresented = true
+                    }
+            } else {
+                Image("nocover")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .onTapGesture {
+                        isPresented = true
+                    }
+            }
+            TextField("Comment", text: $comments)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Button("Upload") {
+                print(comments)
+                print(image!)
+            }
+            .disabled(comments.isEmpty || image == nil)
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+        .background(
+            NavigationLink(
+                destination: FeedsView(),
+                label: { EmptyView() }
+            )
+        )
+        .sheet(isPresented: $isPresented) {
+            ImagePicker(picture: $image)
+        }
     }
 }
 
