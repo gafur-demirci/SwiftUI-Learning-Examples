fb7a0031baf31c578f057ec71f9a52b50c56cab0
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-02 16:29:16 +0300

    Joke model configuration added

--- jokes/jokes/JokesModel.swift
+++ jokes/jokes/JokesModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Joke: Identifiable, Codable {
+    let categories: [String]
+    let createdAt: String
+    let iconURL: String
+    let id, updatedAt: String
+    let url: String
+    let value: String
+
+    enum CodingKeys: String, CodingKey {
+        case categories
+        case createdAt = "created_at"
+        case iconURL = "icon_url"
+        case id
+        case updatedAt = "updated_at"
+        case url, value
+    }
+}
