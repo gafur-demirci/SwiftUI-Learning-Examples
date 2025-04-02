db9072705cd5888ef7649d9c87cd688bb7b67bd3
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-02 16:29:28 +0300

    JokeViewModel configuration added

--- jokes/jokes/JokesViewModel.swift
+++ jokes/jokes/JokesViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Alamofire
+
+class JokesViewModel: ObservableObject {
+    @Published var jokes: [Joke] = []
+    
+    init() {
+        fetchJokes()
+    }
+    
+    func fetchJokes() {
+        AF.request("https://api.chucknorris.io/jokes/random", method: .get).responseDecodable(of:Joke.self) { response in
+            switch response.result {
+                case .success(let joke):
+                    self.jokes += [joke]
+                case .failure(let error):
+                    print("Failed with error: \(error)")
+            }
+        }
+    }
+}
