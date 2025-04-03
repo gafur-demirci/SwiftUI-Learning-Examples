66f65593e655924102e515a0093e8d53e8ece110
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-03 20:11:39 +0300

    Shape and Animation view example added

--- learningSwiftUI/learningSwiftUI/ShapeAndAnimationExamples/ShapeAndAnimationView.swift
+++ learningSwiftUI/learningSwiftUI/ShapeAndAnimationExamples/ShapeAndAnimationView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct ShapeAndAnimationView: View {
+    
+    @State private var firstPercentage : Double = 0
+    @State private var secondPercentage : Double = 0
+    @State private var thirtPercentage : Double = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            CustomCircular(
+                lineWidth: 30,
+                backgroundColor: Color.blue.opacity(0.2),
+                foregroundColor: Color.blue,
+                percentage: firstPercentage
+            )
+            .frame(width: 300, height: 300)
+            .onTapGesture {
+                if firstPercentage == 0 {
+                    firstPercentage = 50
+                } else {
+                    firstPercentage = 0
+                }
+            }
+            CustomCircular(
+                lineWidth: 30,
+                backgroundColor: Color.red.opacity(0.2),
+                foregroundColor: Color.red,
+                percentage: secondPercentage
+            )
+            .frame(width: 200, height: 200)
+            .onTapGesture {
+                if secondPercentage == 0 {
+                    secondPercentage = 75
+                } else {
+                    secondPercentage = 0
+                }
+            }
+            CustomCircular(
+                lineWidth: 30,
+                backgroundColor: Color.green.opacity(0.2),
+                foregroundColor: Color.green,
+                percentage: thirtPercentage
+            )
+            .frame(width: 100, height: 100)
+            .onTapGesture {
+                if thirtPercentage == 0 {
+                    thirtPercentage = 100
+                } else {
+                    thirtPercentage = 0
+                }
+            }
+        }
     }
 }
 
