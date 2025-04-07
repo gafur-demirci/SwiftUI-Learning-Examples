313a7f20baeb74871e90a496d41ace75bdbecc68
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-07 20:02:40 +0300

    Custom button view added

--- Hike/Hike/Component/CustomButtonView.swift
+++ Hike/Hike/Component/CustomButtonView.swift
@@ -9,10 +9,37 @@ import SwiftUI
 
 struct CustomButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [.white, .customGreenLight, .customGreenMedium]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .stroke(
+                    LinearGradient(
+                        gradient: Gradient(colors: [.customGrayLight, .customGrayMedium]),
+                        startPoint: .top,
+                        endPoint: .bottom),
+                    lineWidth: 4)
+            Image(systemName: "figure.hiking")
+                .fontWeight(.black)
+                .font(.system(size: 30))
+                .foregroundStyle(
+                    LinearGradient(
+                        gradient: Gradient(colors: [.customGrayLight, .customGrayMedium]),
+                        startPoint: .top,
+                        endPoint: .bottom)
+                )
+                
+        }
+        .frame(width: 58, height: 58)
     }
 }
 
 #Preview {
     CustomButtonView()
+        .padding()
 }
