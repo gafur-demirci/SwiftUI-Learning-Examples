05aefafa0350c6726087883c52bba81cbc8b3e53
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-07 20:03:34 +0300

    ColorExtension file added for utilize custom color type

--- Hike/Hike/Utility/ColorExtension.swift
+++ Hike/Hike/Utility/ColorExtension.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGreenLight = Color(.colorGreenLight)
+    static let customGreenMedium = Color(.colorGreenMedium)
+    static let customGreenDark = Color(.colorGreenDark)
+    static let customGrayLight = Color(.colorGrayLight)
+    static let customGrayMedium = Color(.colorGrayMedium)
+    static let customIndigoMedium = Color(.colorIndigoMedium)
+    static let customSalmonLight = Color(.colorSalmonLight)
+}
