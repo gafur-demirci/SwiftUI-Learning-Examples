e27845aba1bb41435ede140dae0d8e4e9d68410d
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-07 20:01:43 +0300

    Card view added

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -9,20 +9,87 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        
+        // MARK: Card
+        
         ZStack {
-            Circle()
-                .fill(
+            CustomBackgroundView()
+            VStack {
+                
+                // MARK: - HEADER
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [.customGrayLight, .customGrayMedium]),
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                            )
+                        Spacer()
+                        Button(action: {
+                            // ACTION: Show a Sheet
+                            print(">>> Start hiking")
+                        }, label: {
+                            CustomButtonView()
+                        })
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic(true)
+                        .foregroundColor(.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
+                // MARK: - Main Content
+                
+                
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.colorIndigoMedium, .colorSalmonLight]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                // MARK: - Footer
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.customGreenLight, .customGreenMedium]),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                })
+                .background(
                     LinearGradient(
-                        gradient: Gradient(colors: [.colorIndigoMedium, .colorSalmonLight]),
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
+                        gradient: Gradient(colors: [.customGrayLight, .customGrayMedium]),
+                        startPoint: .top,
+                        endPoint: .bottom
                     )
                 )
-                .frame(width: 256, height: 256)
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
-        }
+                .cornerRadius(40)
+            }
+        } //: Card
+        .frame(width: 320, height: 570)
     }
 }
 
