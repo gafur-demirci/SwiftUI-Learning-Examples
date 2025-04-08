986717992a0941ac5ddacfb0b565d19a815160db
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-08 10:07:56 +0300

    Main content updated with Custom circle view for using background custom circle animation

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -50,15 +50,7 @@ struct CardView: View {
                 // MARK: - MAIN CONTENT
                 
                 ZStack {
-                    Circle()
-                        .fill(
-                            LinearGradient(
-                                gradient: Gradient(colors: [.colorIndigoMedium, .colorSalmonLight]),
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing
-                            )
-                        )
-                        .frame(width: 256, height: 256)
+                    CustomCircleView()
                     Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
