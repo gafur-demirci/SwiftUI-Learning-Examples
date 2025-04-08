f4cfb73a414fc4f48c1064cba52e936719db31a9
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-08 10:06:54 +0300

    Card view inside custom circle animation view added

---
+++ Hike/Hike/Component/CustomCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCircleView.swift
+//  Hike
+//
+//  Created by Abdulgafur Demirci on 8.04.2025.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
