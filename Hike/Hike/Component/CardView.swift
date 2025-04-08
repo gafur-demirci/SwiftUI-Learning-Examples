6327a128718e6db1feb6559f2159574fbd01366b
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-08 12:17:33 +0300

    Sheet view added in custom button pressed and show the Settings view in Card view

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -12,6 +12,7 @@ struct CardView: View {
     
     @State private var imageNumber: Int = 1
     @State private var randomNumber: Int = 1
+    @State private var isShowingSheet: Bool = false
     
     var body: some View {
         // MARK: CARD
@@ -36,9 +37,15 @@ struct CardView: View {
                         Spacer()
                         Button(action: {
                             // ACTION: Show a Sheet
+                            isShowingSheet.toggle()
                             print(">>> Start hiking")
                         }, label: {
                             CustomButtonView()
+                        })
+                        .sheet(isPresented: $isShowingSheet, content: {
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         })
                     }
                     Text("Fun and enjoyable outdoor activity for friends and families.")
---
+++ Hike/Hike/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Hike
+//
+//  Created by Abdulgafur Demirci on 8.04.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
