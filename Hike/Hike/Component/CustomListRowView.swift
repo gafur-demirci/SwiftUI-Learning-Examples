b75ba34d1cd2d7b15a3fddb44d645509639d9916
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-08 12:58:26 +0300

    CustomListRow view added for using in Settings view

--- Hike/Hike/Component/CustomListRowView.swift
+++ Hike/Hike/Component/CustomListRowView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct CustomListRowView: View {
+    
+    // MARK: - PROTERTIES
+    
+    @State var rowLabel: String
+    @State var rowIcon: String
+    @State var rowContent: String? = nil
+    @State var rowTintColor: Color
+    @State var rowLinkLabel: String? = nil
+    @State var rowLinkDestination: String? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LabeledContent {
+            if rowContent != nil {
+                Text(rowContent!)
+                    .foregroundStyle(.primary)
+                    .fontWeight(.heavy)
+            } else if (rowLinkLabel != nil) && (rowLinkDestination != nil) {
+                Link(rowLinkLabel!, destination: URL(string: (rowLinkDestination!))!)
+                    .foregroundStyle(.pink)
+                    .fontWeight(.heavy)
+            }
+            else {
+                EmptyView()
+            }
+        } label: {
+            // Label
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: 30, height: 30)
+                        .foregroundStyle(rowTintColor)
+                    Image(systemName: rowIcon)
+                        .foregroundStyle(.white)
+                        .fontWeight(.semibold)
+                }
+                Text(rowLabel)
+                    .fontWeight(.medium)
+            }
+        }
     }
 }
 
 #Preview {
-    CustomListRowView()
+    List {
+        CustomListRowView(rowLabel: "Hike", rowIcon: "apps.iphone", rowContent: "Application", rowTintColor: .blue)
+    }
 }
