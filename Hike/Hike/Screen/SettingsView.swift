5c9566bfc5b08886da6412720d1ed9dbba39c201
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-08 12:18:00 +0300

    Settings view added and configured texts

--- Hike/Hike/Screen/SettingsView.swift
+++ Hike/Hike/Screen/SettingsView.swift
@@ -9,7 +9,54 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            
+            // MARK: - SECTION: HEADER
+            Section {
+                HStack {
+                    Spacer()
+                    Image(systemName: "laurel.leading")
+                        .font(.system(size: 80, weight: .black))
+                    VStack(spacing: -10) {
+                        Text("Hike")
+                            .font(.system(size: 66, weight: .black))
+                        Text("Editors' Choice")
+                            .fontWeight(.medium)
+                    }
+                    Image(systemName: "laurel.trailing")
+                        .font(.system(size: 80, weight: .black))
+                    Spacer()
+                }
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [.customGreenLight, .customGreenMedium, .customGreenDark],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .padding(.top, 8)
+                
+                VStack(spacing: 8) {
+                    Text("Where can you find \nperfect tracks?")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                    Text("The hike which looks gorgeous in photos but is even better once you are  actually there. The hike that you hope to do again somaday. \nFind the best day hikes in the app.")
+                        .font(.footnote)
+                        .italic()
+                    Text("Dust off the boots! It's time for a walk.")
+                        .fontWeight(.heavy)
+                        .foregroundStyle(Color.customGreenMedium)
+                }
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 16)
+                .frame(maxWidth: .infinity)
+            } //: HEADER
+            .listRowSeparator(Visibility.hidden)
+            // MARK: - SECTION: ICONS
+            
+            // MARK: - SECTION: ABOUT
+            
+        } //: LIST
     }
 }
 
