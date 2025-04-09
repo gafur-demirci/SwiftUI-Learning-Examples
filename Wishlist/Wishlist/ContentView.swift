789759116b5f2f77c8fc53ae8c2cc8d51847b4f3
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-09 10:46:13 +0300

    New preview section added for use dummy SwiftData preview in example

--- Wishlist/Wishlist/ContentView.swift
+++ Wishlist/Wishlist/ContentView.swift
@@ -19,7 +19,20 @@ struct ContentView: View {
     }
 }
 
-#Preview {
+#Preview("List with Sample Data") {
+    let container = try! ModelContainer(for: Wish.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    container.mainContext.insert(Wish(title: "Master SwiftData"))
+    container.mainContext.insert(Wish(title: "Buy a new iPhone"))
+    container.mainContext.insert(Wish(title: "Practise latin dances"))
+    container.mainContext.insert(Wish(title: "Travel to Europe"))
+    container.mainContext.insert(Wish(title: "Make a positive impact"))
+    
+    return ContentView()
+        .modelContainer(container)
+}
+
+#Preview("Empty List") {
     ContentView()
         .modelContainer(for: Wish.self, inMemory: true)
 }
