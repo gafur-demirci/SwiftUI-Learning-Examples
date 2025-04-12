4c3937bf0e70838b1d9c148da9e954140a2f9308
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-12 16:19:51 +0300

    Empty item state show to the user UI view added by CustomContentUnavailableView

--- Paws/Paws/CustomContentIUnavailableView.swift
+++ Paws/Paws/CustomContentIUnavailableView.swift
@@ -7,12 +7,32 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct CustomContentIUnavailableView: View {
+    
+    var title: String
+    var icon: String
+    var description: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ContentUnavailableView {
+            Image(systemName: icon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 96)
+            
+            Text(title)
+                .font(.title)
+        } description: {
+            Text(description)
+        }
+        .foregroundStyle(.tertiary)
     }
 }
 
 #Preview {
-    SwiftUIView()
+    CustomContentIUnavailableView(
+        title: "No Photo",
+        icon: "cat.circle",
+        description: "Add a photo to get started."
+    )
 }
