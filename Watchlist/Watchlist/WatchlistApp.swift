3523effd62bbd97aee0b556b6201aec6a0dd78bf
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-12 20:29:26 +0300

    Watchlist initial files and appIcon set added

---
+++ Watchlist/Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Abdulgafur Demirci on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
