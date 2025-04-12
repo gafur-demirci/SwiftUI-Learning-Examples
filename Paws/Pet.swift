a7aa51191caeab51ee2e44b99897c08e748951d9
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-12 16:17:13 +0300

    Pet model added and configured initial sampla data

--- Paws/Pet.swift
+++ Paws/Pet.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class Pet {
+    var  name: String
+    @Attribute(.externalStorage) var photo: Data?
+    
+    init(name: String, photo: Data? = nil) {
+        self.name = name
+        self.photo = photo
+    }
+}
+
+extension Pet {
+    @MainActor
+    static var preivew: ModelContainer {
+        let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try! ModelContainer(for: Pet.self, configurations: configuration)
+        
+        container.mainContext.insert(Pet(name: "Rexy"))
+        container.mainContext.insert(Pet(name: "Bella"))
+        container.mainContext.insert(Pet(name: "Charlie"))
+        container.mainContext.insert(Pet(name: "Daisy"))
+        container.mainContext.insert(Pet(name: "Fido"))
+        container.mainContext.insert(Pet(name: "Gus"))
+        container.mainContext.insert(Pet(name: "Mimi"))
+        container.mainContext.insert(Pet(name: "Luna"))
+        
+        return container
+    }
+}
