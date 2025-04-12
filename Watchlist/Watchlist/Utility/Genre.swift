6c5a2c2cfcc802001821758d0ebdb826a7c5f3cc
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-12 21:15:36 +0300

    Genre enum added and int to string switch extension configured

--- Watchlist/Watchlist/Utility/Genre.swift
+++ Watchlist/Watchlist/Utility/Genre.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+enum Genre: Int, Codable, CaseIterable, Identifiable {
+    var id: Int { rawValue }
+    
+    case action         = 1
+    case comedy         = 2
+    case crime          = 3
+    case documentary    = 4
+    case drama          = 5
+    case fantasy        = 6
+    case kids           = 7
+    case musical        = 8
+    case scifi          = 9
+    case romance        = 10
+    case thiller        = 11
+    case western        = 12
+}
+
+extension Genre {
+    var name: String {
+        switch self {
+        case .action:         return "Action"
+        case .comedy:         return "Comedy"
+        case .crime:          return "Crime"
+        case .documentary:    return "Documentary"
+        case .drama:          return "Drama"
+        case .fantasy:        return "Fantasy"
+        case .kids:           return "Kids"
+        case .musical:        return "Musical"
+        case .scifi:          return "Sci-Fi"
+        case .romance:        return "Romance"
+        case .thiller:        return "Thriller"
+        case .western:        return "Western"
+        }
+    }
+}
