3d4ec0e5487d83a5fa4fdb2526bbdd1734edfc53
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-13 17:20:04 +0300

    Restart app initialized

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Abdulgafur Demirci on 13.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
