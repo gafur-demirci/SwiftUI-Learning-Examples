2840452a468e40d8bde100709b001be799a58e59
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-13 15:46:47 +0300

    Some properties added for custom PageTabView view

--- Watchlist/Watchlist/Component/PageTabView.swift
+++ Watchlist/Watchlist/Component/PageTabView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct PageTabView: View {
+    
+    var icon: String
+    var description: String
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
