919963f7c989982565e1009a90fe74299c2a9c67
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-13 15:45:47 +0300

    Custom Reuseable ButtonImageView component UI configuration added

--- Watchlist/Watchlist/Component/ButtonImageView.swift
+++ Watchlist/Watchlist/Component/ButtonImageView.swift
@@ -12,7 +12,16 @@ struct ButtonImageView: View {
     let symbolName: String
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: symbolName)
+            .resizable()
+            .scaledToFit()
+            .foregroundStyle(.blue.gradient)
+            .padding(8)
+            .background(
+                Circle()
+                    .fill(.ultraThickMaterial)
+            )
+            .frame(width: 80)
     }
 }
 
