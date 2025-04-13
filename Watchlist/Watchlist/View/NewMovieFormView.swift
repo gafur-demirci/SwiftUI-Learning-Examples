1a70a03d5998601a99eb267343d766948a9e8171
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-13 15:47:52 +0300

    Some properties added for using in NewMovieFormView

--- Watchlist/Watchlist/View/NewMovieFormView.swift
+++ Watchlist/Watchlist/View/NewMovieFormView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct NewMovieFormView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title: String = ""
+    @State private var selectedGenre: Genre = .kids
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
