7622ef79db2db33f4b0975e30aa1338e719d359d
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-13 15:42:39 +0300

    ContentView configured for Empty state and with data show list UI

--- Watchlist/Watchlist/View/ContentView.swift
+++ Watchlist/Watchlist/View/ContentView.swift
@@ -20,11 +20,59 @@ struct ContentView: View {
     @State private var isShowingAlert: Bool = false
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            if !movies.isEmpty {
+                Section(content: {
+                    ForEach(movies) { movie in
+                        HStack {
+                            Text(movie.title)
+                                .font(.title.weight(.light))
+                                .padding(.vertical, 2)
+                            
+                            Spacer()
+                            
+                            Text(movie.genre.name)
+                                .font(.footnote.weight(.medium))
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 3)
+                                .background(
+                                    Capsule()
+                                        .stroke(lineWidth: 1)
+                                )
+                                .foregroundStyle(.tertiary)
+                            
+                        } //: LIST ROW
+                        .swipeActions(edge: .trailing) {
+                            Button(role: .destructive, action: {
+                                withAnimation {
+                                    modelContext.delete(movie)
+                                }
+                            }, label: {
+                                Label("Delete", systemImage: "trash")
+                            })
+                        }
+                    }
+                }, header: {
+                    VStack {
+                        
+                        Text("Wathclist")
+                            .font(.largeTitle.weight(.black))
+                            .foregroundStyle(.blue.gradient)
+                            .padding()
+                        
+                        HStack {
+                            Label("Title", systemImage: "movieclapper")
+                            Spacer()
+                            Label("Genre", systemImage: "tag")
+                        }
+                    }
+                })
+            }
+        } //: LIST
+        .overlay {
+            if movies.isEmpty {
+                EmptyListView()
+            }
         }
         .padding()
     }
