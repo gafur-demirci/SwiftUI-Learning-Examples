8ebd58f277635fc4075009e0db0375ba87e9c2e2
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-13 15:48:43 +0300

    NewMovieFormView UI configurations and add movie with using SwiftData useability functionality added

--- Watchlist/Watchlist/View/NewMovieFormView.swift
+++ Watchlist/Watchlist/View/NewMovieFormView.swift
@@ -18,7 +18,63 @@ struct NewMovieFormView: View {
     @State private var selectedGenre: Genre = .kids
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            List {
+                // MARK: - HEADER
+                Text("What to Watch")
+                    .font(.largeTitle.weight(.black))
+                    .foregroundStyle(.blue.gradient)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                    .padding(.vertical)
+                
+                // MARK: - TITLE
+                TextField("Title", text: $title)
+                    .textFieldStyle(.roundedBorder)
+                    .font(.largeTitle.weight(.light))
+                
+                // MARK: - GENRE
+                Picker("Genre", selection: $selectedGenre) {
+                    ForEach(Genre.allCases) { genre in
+                        Text(genre.name)
+                            .tag(genre)
+                    }
+                }
+                
+                // MARK: - SAVE BUTTON
+                Button(action: {
+                    if title.isEmpty || title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty { return }
+                    addNewMovie()
+                    dismiss()
+                }, label: {
+                    Text("Save")
+                        .font(.title2.weight(.medium))
+                        .frame(maxWidth: .infinity)
+                })
+                .buttonStyle(.borderedProminent)
+                .controlSize(.extraLarge)
+                .buttonBorderShape(.roundedRectangle)
+                .disabled(title.isEmpty || title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
+                
+                // MARK: - CANCEL BUTTON
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Text("Close")
+                        .frame(maxWidth: .infinity)
+                })
+                
+            } //: LIST
+            .listRowSeparator(.hidden)
+        } //: FORM
+    }
+    
+    // MARK: - FUNCTIONS
+    
+    private func addNewMovie() {
+        let newMovie = Movie(title: title, genre: selectedGenre)
+        modelContext.insert(newMovie)
+        title = ""
+        selectedGenre = .kids
     }
 }
 
