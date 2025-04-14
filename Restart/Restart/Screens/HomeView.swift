4b50aaef885c832a4cfcc0617389c4deb18f5516
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-14 18:07:42 +0300

    HomeView UI configuration added

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -16,14 +16,47 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            
+            // MARK: - HEADER
+            
+            Spacer()
+            
+            ZStack {
+                
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
+            
+            // MARK: - CENTER
+            
+            Text("The time that  leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: - FOOTER
+            
+            Spacer()
             
             Button(action: {
                 isOnboardingViewActive = true
             }, label: {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
                 Text("Restart")
-            })
+                    .font(.title3)
+                    .fontDesign(.rounded)
+                    .fontWeight(.bold)
+            }) //: BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         } //: VSTACK
     } //: BODY
 }
---
+++ Restart/Restart/Views/CircleGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Abdulgafur Demirci on 14.04.2025.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleGroupView()
+}
