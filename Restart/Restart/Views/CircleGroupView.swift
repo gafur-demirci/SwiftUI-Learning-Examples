eaf6b795583f26d4bbbe2cb909ad22c6249dcf02
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-15 21:00:17 +0300

    Animation modifiers added in custom component by CircleGroupView

--- Restart/Restart/Views/CircleGroupView.swift
+++ Restart/Restart/Views/CircleGroupView.swift
@@ -24,6 +24,13 @@ struct CircleGroupView: View {
                 .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
                 .frame(width: 260, height: 260, alignment: .center)
         } //: ZSTACK
+        .blur(radius: isAnimating ? 0 : 10)
+        .opacity(isAnimating ? 1 : 0)
+        .scaleEffect(isAnimating ? 1 : 0.5)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+        .onAppear {
+            isAnimating = true
+        }
     }
 }
 
