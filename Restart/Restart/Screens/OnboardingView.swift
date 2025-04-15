14f091757d6ecbd088e894e9d7937beae11546c4
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-15 18:20:29 +0300

    Footer button add gesture to use drag and drop to the dragging this button and animating background and some actions added

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -108,10 +108,25 @@ struct OnboardingView: View {
                         }
                         .foregroundStyle(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
-                        
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if(gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80){
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    withAnimation(.easeOut(duration: 0.4)) {
+                                        if buttonOffset > buttonWidth / 2 {
+                                            buttonOffset = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        } else {
+                                            buttonOffset = 0
+                                        }
+                                    }
+                                }
+                        ) //: GESTURE
                         Spacer()
                     } //: HSTACK
                 } //: FOOTER
