0ec275f4b0505a632e8425a579fad79f0e3ebc65
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-15 18:18:03 +0300

    Button background capsule frame size controlled animated button location

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -89,7 +89,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                     }
                     
