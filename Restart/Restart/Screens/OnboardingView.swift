eb392dae80aa86484332b4dfa1ecdc4205df0537
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-17 20:58:05 +0300

    Onboarding header title, centered image and custom component animation modifier configuration added

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -16,6 +16,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var headerTitle: String = "Share."
     
     // MARK: - BODY
     
@@ -30,9 +33,11 @@ struct OnboardingView: View {
                 
                 Spacer()
                 VStack {
-                    Text("Share.")
+                    Text(headerTitle)
                         .font(.system(size: 60, weight: .heavy))
                         .foregroundStyle(.white)
+                        .transition(.opacity) // without id modifier not run this modifier
+                        .id(headerTitle)
                     Text("""
                         It's not how much we give but how much love we put intu giving.
                     """)
@@ -50,14 +55,49 @@ struct OnboardingView: View {
                 
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1 )
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
-                    
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20))) // rotate the image movement
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation(.linear(duration: 0.25), {
+                                            indicatorOpacity = 0
+                                            headerTitle = "Give."
+                                        })
+                                    }
+                                }
+                                .onEnded({ _ in
+                                    imageOffset = .zero
+                                    withAnimation(.linear(duration: 0.25), {
+                                        indicatorOpacity = 1
+                                        headerTitle = "Share."
+                                    })
+                                })
+                        ) //: GESTURE
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                 } //: CENTER
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundStyle(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    , alignment: .bottom
+                )
                 
                 Spacer()
                 
