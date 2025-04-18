9f09795e7ab7efaa13444b4cad82a85788294b94
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-18 10:11:51 +0300

    Utility func added for playing to custom sound file in app UI actions

--- Restart/Restart/Utilities/AudioPlayer.swift
+++ Restart/Restart/Utilities/AudioPlayer.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    guard let path = Bundle.main.path(forResource: sound, ofType: type) else { return }
+    do {
+        audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path), fileTypeHint: type)
+        audioPlayer?.play()
+    } catch {
+        print("Error playing sound: \(error)")
+    }
+}
