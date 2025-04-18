f08cb6673d06d50a01dcad9d72bc2e951f866bd6
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-18 22:05:49 +0300

    Pinch app initial files and folders added

---
+++ Pinch/Pinch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Abdulgafur Demirci on 18.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pinch/Pinch/View/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Abdulgafur Demirci on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
