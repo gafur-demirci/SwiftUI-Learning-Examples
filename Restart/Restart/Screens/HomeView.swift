afee6879d2db3f20a204485fef1d719b1603472c
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-18 10:10:45 +0300

    Custom sound function triggered press to restart button

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -53,6 +53,7 @@ struct HomeView: View {
             
             Button(action: {
                 withAnimation {
+                    playSound(sound: "success", type: "m4a")
                     isOnboardingViewActive = true
                 }
             }, label: {
