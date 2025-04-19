9806d8aa8224b0f351ed8fbe2f9c810682955db0
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-19 17:58:32 +0300

    Image when clicked double tap and scale to zoom with animation added

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -15,13 +15,38 @@ struct ContentView: View {
     @State private var imageScale: CGFloat = 1
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2) {
+                        if self.imageScale == 1 {
+                            withAnimation(.spring) {
+                                self.imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring) {
+                                self.imageScale = 1
+                            }
+                        }
+                    }
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    self.isAnimating = true
+                }
+            }
+        } //: NAVIGATION
     }
 }
 
