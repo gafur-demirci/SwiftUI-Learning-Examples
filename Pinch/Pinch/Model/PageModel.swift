9a09da5e7a74a8dc74389ababe11081c2a18fb18
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-20 20:43:55 +0300

    Page model configuration added with extension to return thumbnail image name

--- Pinch/Pinch/Model/PageModel.swift
+++ Pinch/Pinch/Model/PageModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Page: Identifiable {
+    let id: Int
+    let imageName: String
+}
+
+extension Page {
+    var thumbnailName: String {
+        return "thumb-\(imageName)"
+    }
+}
