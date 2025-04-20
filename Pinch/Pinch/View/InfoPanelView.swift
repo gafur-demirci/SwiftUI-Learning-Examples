82a4e2f064a82cf513d1cba84633c89b4c395930
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-20 17:22:53 +0300

    InfoPanelView properties added and preview configured

--- Pinch/Pinch/View/InfoPanelView.swift
+++ Pinch/Pinch/View/InfoPanelView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct InfoPanelView: View {
+    
+    // MARK: - PROPERTIES
+    
+    var scale: CGFloat
+    var offset: CGSize
+    @State private var isInfoPanelVisible: Bool = false
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    InfoPanelView()
+    InfoPanelView(scale: 1.0, offset: .zero)
 }
