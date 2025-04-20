770d619e061dbeabad07bd16d3783b178aa25769
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-20 10:58:16 +0300

    Drag gesture added for image dragging on the screen

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    // MARK: PROPERTIES
+    // MARK: - PROPERTIES
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
@@ -25,6 +25,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2) {
@@ -38,6 +39,19 @@ struct ContentView: View {
                             }
                         }
                     }
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(DragGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1)) {
+                                self.imageOffset = value.translation
+                            }
+                        }
+                        .onEnded { _ in
+                            if imageScale <= 1 {
+                                resetImageState()
+                            }
+                            
+                        })
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
