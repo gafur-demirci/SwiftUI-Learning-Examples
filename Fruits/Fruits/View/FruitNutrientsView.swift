b3ab913a2d08fc6ec88dc59eac591df979704d0f
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-24 12:52:13 +0300

    FruitNutrient view added and UI configurated

--- Fruits/Fruits/View/FruitNutrientsView.swift
+++ Fruits/Fruits/View/FruitNutrientsView.swift
@@ -1,20 +1,40 @@
-// MARK: - PROPERTIES
-  
-  var fruit: Fruit
-  let nutrients: [String] = ["Energy", "Sugar", "Fat", "Protein", "Vitamins", "Minerals"]
+//
+//  FruitNutrientsView.swift
+//  Fruits
+//
+//  Created by Abdulgafur Demirci on 22.04.2025.
+//
 
-  // MARK: - BODY
+import SwiftUI
 
-  var body: some View {
-    GroupBox() {
-      DisclosureGroup("Nutritional value per 100g") {
-        ForEach(0..<nutrients.count, id: \.self) { item in
-          Divider().padding(.vertical, 2)
-          
-          HStack {
-            Group {
-              Image(systemName: "info.circle")
-              Text(nutrients[item])
+struct FruitNutrientsView: View {
+    // MARK: - PROPERTIES
+    
+    var fruit: Fruit
+    let nutrients: [String] = ["Energy", "Sugar", "Fat", "Protein", "Vitamins", "Minerals"]
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        GroupBox() {
+            DisclosureGroup("Nutritional value per 100g") {
+                ForEach(0..<nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 2)
+                    
+                    HStack {
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(Font.system(.body).bold())
+                        
+                        Spacer(minLength: 25)
+                        
+                        Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
             }
         } //: BOX
     }
