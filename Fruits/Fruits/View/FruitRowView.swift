4fd398f1a2b4d55703217e7a09f3cabff3cc0f67
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-24 12:44:34 +0300

    Some properties added

--- Fruits/Fruits/View/FruitRowView.swift
+++ Fruits/Fruits/View/FruitRowView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct FruitRowView: View {
+    // MARK: - PROPERTIES
+    
+    var fruit: Fruit
+
+    // MARK: - BODY
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
