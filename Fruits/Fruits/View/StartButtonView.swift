9325bee8cebf0cbd260256e6bb11563b0ca60876
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-24 12:45:33 +0300

    Some properties added

--- Fruits/Fruits/View/StartButtonView.swift
+++ Fruits/Fruits/View/StartButtonView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct StartButtonView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
