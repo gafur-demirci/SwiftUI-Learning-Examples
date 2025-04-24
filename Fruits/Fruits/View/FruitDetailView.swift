6ff7643752e79b9f84d0772decac63799b1615df
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-24 12:48:44 +0300

    FruitDetail view UI configurations added

--- Fruits/Fruits/View/FruitDetailView.swift
+++ Fruits/Fruits/View/FruitDetailView.swift
@@ -15,7 +15,50 @@ struct FruitDetailView: View {
     // MARK: - BODY
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationView {
+        ScrollView(.vertical, showsIndicators: false) {
+          VStack(alignment: .center, spacing: 20) {
+            // HEADER
+            FruitHeaderView(fruit: fruit)
+            
+            VStack(alignment: .leading, spacing: 20) {
+              // TITLE
+              Text(fruit.title)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(fruit.gradientColors[1])
+              
+              // HEADLINE
+              Text(fruit.headline)
+                .font(.headline)
+                .multilineTextAlignment(.leading)
+              
+              // NUTRIENTS
+              FruitNutrientsView(fruit: fruit)
+              
+              // SUBHEADLINE
+              Text("Learn more about \(fruit.title)".uppercased())
+                .fontWeight(.bold)
+                .foregroundColor(fruit.gradientColors[1])
+              
+              // DESCRIPTION
+              Text(fruit.description)
+                .multilineTextAlignment(.leading)
+              
+              // LINK
+              SourceLinkView()
+                .padding(.top, 10)
+                .padding(.bottom, 40)
+            } //: VSTACK
+            .padding(.horizontal, 20)
+            .frame(maxWidth: 640, alignment: .center)
+          } //: VSTACK
+          .navigationBarTitle(fruit.title, displayMode: .inline)
+          .navigationBarHidden(true)
+        } //: SCROLL
+        .edgesIgnoringSafeArea(.top)
+      } //: NAVIGATION
+      .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
