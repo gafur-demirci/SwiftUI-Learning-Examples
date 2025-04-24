ed84a140938624082d42e903fbcd3a49eedbf41d
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-24 12:46:06 +0300

    Some properties added

--- Fruits/Fruits/View/SettingsLabelView.swift
+++ Fruits/Fruits/View/SettingsLabelView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct SettingsLabelView: View {
+    // MARK: - PROPERTIES
+    
+    var labelText: String
+    var labelImage: String
+
+    // MARK: - BODY
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
