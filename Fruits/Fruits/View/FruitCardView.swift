f92f46bd5a99b30b8c49421f19e3da6b9c85c60b
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-24 12:47:51 +0300

    FruitCard view UI configurations added

--- Fruits/Fruits/View/FruitCardView.swift
+++ Fruits/Fruits/View/FruitCardView.swift
@@ -15,8 +15,47 @@ struct FruitCardView: View {
     @State private var isAnimating: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image(fruit.image)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .red.opacity(0.15), radius: 8, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                
+                // FRUIT: TITLE
+                Text(fruit.title)
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: .red.opacity(0.15), radius: 2, x: 2, y: 2)
+                
+                // FRUIT: HEADLINE
+                Text(fruit.headline)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                // BUTTON: START
+                StartButtonView()
+            } //: VSTACK
+        } //: ZSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(
+            LinearGradient(
+                gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom
+            )
+        )
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
+    } //: BODY
 }
 
 #Preview {
