3bf0d8b8e7764f64bfd7b3e63adda92390add379
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-24 12:44:44 +0300

    Some properties added

--- Fruits/Fruits/View/OnboardingView.swift
+++ Fruits/Fruits/View/OnboardingView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
