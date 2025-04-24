52469904608eae5de5ecd08ab5e6ec149efc4387
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-04-24 12:45:48 +0300

    Some properties added

--- Fruits/Fruits/View/SettingsView.swift
+++ Fruits/Fruits/View/SettingsView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
+    
+    // MARK: - BODY
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
