8e1098eb9b6aaf585f7853f8a303a3d2d0f82738
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:43:43 +0300

    Section view UI configurations added

--- Touchdown/Touchdown/View/Home/SectionView.swift
+++ Touchdown/Touchdown/View/Home/SectionView.swift
@@ -15,7 +15,19 @@ struct SectionView: View {
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack(spacing: 0) {
+        Spacer()
+        
+        Text("Categories".uppercased())
+          .font(.footnote)
+          .fontWeight(.bold)
+          .foregroundColor(.white)
+          .rotationEffect(Angle(degrees: rotateClockwise ? 90 : -90))
+        
+        Spacer()
+      } //: VSTACK
+      .background(colorGray.cornerRadius(12))
+      .frame(width: 85)
     }
 }
 
