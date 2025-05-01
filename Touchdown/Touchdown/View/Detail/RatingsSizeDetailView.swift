eb3a86b5580d6aadef35a6d02abca0c4d52e73ba
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:35:11 +0300

    Some properties added

--- Touchdown/Touchdown/View/Detail/RatingsSizeDetailView.swift
+++ Touchdown/Touchdown/View/Detail/RatingsSizeDetailView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct RatingsSizeDetailView: View {
+    // MARK: - PROPERTY
+    
+    let sizes: [String] = ["XS", "S", "M", "L", "XL"]
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
