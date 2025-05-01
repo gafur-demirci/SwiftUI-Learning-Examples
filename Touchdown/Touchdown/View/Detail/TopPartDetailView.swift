160dd1956315575fc47a6852bffe53c77f89a82f
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:40:39 +0300

    TopPartDetail view UI configurations added

--- Touchdown/Touchdown/View/Detail/TopPartDetailView.swift
+++ Touchdown/Touchdown/View/Detail/TopPartDetailView.swift
@@ -16,7 +16,32 @@ struct TopPartDetailView: View {
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      HStack(alignment: .center, spacing: 6, content: {
+        // PRICE
+        VStack(alignment: .leading, spacing: 6, content: {
+          Text("Price")
+            .fontWeight(.semibold)
+          
+          Text(shop.selectedProduct?.formattedPrice ?? sampleProduct.formattedPrice)
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .scaleEffect(1.35, anchor: .leading)
+        })
+        .offset(y: isAnimating ? -50 : -75)
+        
+        Spacer()
+        
+        // PHOTO
+        Image(shop.selectedProduct?.image ?? sampleProduct.image)
+          .resizable()
+          .scaledToFit()
+          .offset(y: isAnimating ? 0 : -35)
+      }) //: HSTACK
+      .onAppear(perform: {
+        withAnimation(.easeOut(duration: 0.75)) {
+          isAnimating.toggle()
+        }
+      })
     }
 }
 
