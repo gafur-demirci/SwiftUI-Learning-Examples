3fdfd8ce2390f3043d2c8ccb32a7ffb6b53ebc18
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:41:43 +0300

    CategoryItem view UI configurations added

--- Touchdown/Touchdown/View/Home/CategoryItemView.swift
+++ Touchdown/Touchdown/View/Home/CategoryItemView.swift
@@ -15,7 +15,28 @@ struct CategoryItemView: View {
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      Button(action: {}, label: {
+        HStack(alignment: .center, spacing: 6) {
+          Image(category.image)
+            .renderingMode(.template)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 30, height: 30, alignment: .center)
+            .foregroundColor(.gray)
+          
+          Text(category.name.uppercased())
+            .fontWeight(.light)
+            .foregroundColor(.gray)
+          
+          Spacer()
+        } //: HSTACK
+        .padding()
+        .background(Color.white.cornerRadius(12))
+        .background(
+          RoundedRectangle(cornerRadius: 12)
+            .stroke(Color.gray, lineWidth: 1)
+        )
+      }) //: BUTTON
     }
 }
 
