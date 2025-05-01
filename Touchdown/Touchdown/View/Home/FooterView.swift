e944d5e9d2e54fa673aa25f310eae4d1afbd6ccc
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:06:31 +0300

    Footer view UI configuration added

--- Touchdown/Touchdown/View/Home/FooterView.swift
+++ Touchdown/Touchdown/View/Home/FooterView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct FooterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 10) {
+          Text("We offer the most cutting edge, comfortable, lightweight and durable football helmets in the market at affordable prices.")
+            .foregroundColor(.gray)
+            .multilineTextAlignment(.center)
+            .layoutPriority(2)
+          
+          Image("logo-lineal")
+            .renderingMode(.template)
+            .foregroundColor(.gray)
+            .layoutPriority(0)
+          
+          Text("Copyright © Robert Petras\nAll right reserved")
+            .font(.footnote)
+            .fontWeight(.bold)
+            .foregroundColor(.gray)
+            .multilineTextAlignment(.center)
+            .layoutPriority(1)
+        } //: VSTACK
+        .padding()
     }
 }
 
