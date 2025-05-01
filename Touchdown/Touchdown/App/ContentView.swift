eac4ea3f2fb243458db139bca6024184e6690adf
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:33:27 +0300

    ContentView UI configurations added

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -15,13 +15,57 @@ struct ContentView: View {
     // MARK: - BODY
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      ZStack {
+        if shop.showingProduct == false && shop.selectedProduct == nil {
+          VStack(spacing: 0) {
+            NavigationBarView()
+              .padding(.horizontal, 15)
+              .padding(.bottom)
+              .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+              .background(Color.white)
+              .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+            
+            ScrollView(.vertical, showsIndicators: false, content: {
+              VStack(spacing: 0) {
+                FeaturedTabView()
+                  .frame(height: UIScreen.main.bounds.width / 1.475) // This will fix the layout rendering priority issue by using the screen's aspect ratio.
+                  .padding(.vertical, 10)
+                
+                CategoryGridView()
+                
+                TitleView(title: "Helmets")
+                
+                LazyVGrid(columns: gridLayout, spacing: 15, content: {
+                  ForEach(products) { product in
+                    ProductItemView(product: product)
+                      .onTapGesture {
+                        feedback.impactOccurred()
+                        
+                        withAnimation(.easeOut) {
+                          shop.selectedProduct = product
+                          shop.showingProduct = true
+                        }
+                      }
+                  } //: LOOP
+                }) //: GRID
+                  .padding(15)
+                
+                TitleView(title: "Brands")
+                
+                BrandGridView()
+                
+                FooterView()
+                  .padding(.horizontal)
+              } //: VSTACK
+            }) //: SCROLL
+            
+          } //: VSTACK
+          .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        } else {
+          ProductDetailView()
         }
-        .padding()
+      } //: ZSTACK
+      .ignoresSafeArea(.all, edges: .top)
     }
 }
 
