0177006a1b3bfa9174cdcfd0b5327da04fc1bcb5
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:36:52 +0300

    Some properties added

--- Touchdown/Touchdown/View/Home/FeaturedItemView.swift
+++ Touchdown/Touchdown/View/Home/FeaturedItemView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct FeaturedItemView: View {
+    // MARK: - PROPERTY
+    
+    let player: Player
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
