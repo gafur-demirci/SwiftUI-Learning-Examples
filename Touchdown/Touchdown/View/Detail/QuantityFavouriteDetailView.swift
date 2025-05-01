352eef83e865e8b5b784b8a84a4c8f3ae2b259d0
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:40:06 +0300

    QuantityFavouriteDetail view UI configurations added

--- Touchdown/Touchdown/View/Detail/QuantityFavouriteDetailView.swift
+++ Touchdown/Touchdown/View/Detail/QuantityFavouriteDetailView.swift
@@ -15,7 +15,41 @@ struct QuantityFavouriteDetailView: View {
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      HStack(alignment: .center, spacing: 6, content: {
+        Button(action: {
+          if counter > 0 {
+            feedback.impactOccurred()
+            counter -= 1
+          }
+        }, label: {
+          Image(systemName: "minus.circle")
+        })
+        
+        Text("\(counter)")
+          .fontWeight(.semibold)
+          .frame(minWidth: 36)
+        
+        Button(action: {
+          if counter < 100 {
+            feedback.impactOccurred()
+            counter += 1
+          }
+        }, label: {
+          Image(systemName: "plus.circle")
+        })
+        
+        Spacer()
+        
+        Button(action: {
+          feedback.impactOccurred()
+        }, label: {
+          Image(systemName: "heart.circle")
+            .foregroundColor(.pink)
+        })
+      }) //: HSTACK
+      .font(.system(.title, design: .rounded))
+      .foregroundColor(.black)
+      .imageScale(.large)
     }
 }
 
