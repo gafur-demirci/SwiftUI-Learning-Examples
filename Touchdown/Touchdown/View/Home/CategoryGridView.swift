de2060451f4130b02df0171fc8914334abee4b6c
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:41:28 +0300

    CategoryGrid view UI configurations added

--- Touchdown/Touchdown/View/Home/CategoryGridView.swift
+++ Touchdown/Touchdown/View/Home/CategoryGridView.swift
@@ -13,7 +13,21 @@ struct CategoryGridView: View {
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ScrollView(.horizontal, showsIndicators: false, content: {
+        LazyHGrid(rows: gridLayout, alignment: .center, spacing: columnSpacing, pinnedViews: [], content: {
+          Section(
+            header: SectionView(rotateClockwise: false),
+            footer: SectionView(rotateClockwise: true)
+          ) {
+            ForEach(categories) { category in
+              CategoryItemView(category: category)
+            }
+          }
+        }) //: GRID
+        .frame(height: 140)
+        .padding(.horizontal, 15)
+        .padding(.vertical, 10)
+      }) //: SCROLL
     }
 }
 
