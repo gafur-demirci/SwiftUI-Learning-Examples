238d20499a16c0c393e5edb5d1c5fcbcfada4dd8
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:47:08 +0300

    CustomShape configuration added for detail view background view

--- Touchdown/Touchdown/Utility/CustomShape.swift
+++ Touchdown/Touchdown/Utility/CustomShape.swift
@@ -5,4 +5,20 @@
 //  Created by Abdulgafur Demirci on 1.05.2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomShape: Shape {
+  func path(in rect: CGRect) -> Path {
+    let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 35, height: 35))
+    
+    return Path(path.cgPath)
+  }
+}
+
+struct CustomShape_Previews: PreviewProvider {
+  static var previews: some View {
+    CustomShape()
+      .previewLayout(.fixed(width: 428, height: 120))
+      .padding()
+  }
+}
