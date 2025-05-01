f863f8dfcae7db8b169be15c1d84838fde43c6c3
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:43:02 +0300

    NavigationBar view UI configurations added

--- Touchdown/Touchdown/View/Home/NavigationBarView.swift
+++ Touchdown/Touchdown/View/Home/NavigationBarView.swift
@@ -15,7 +15,39 @@ struct NavigationBarView: View {
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      HStack {
+        Button(action: {}, label: {
+          Image(systemName: "magnifyingglass")
+            .font(.title)
+            .foregroundColor(.black)
+        }) //: BUTTON
+        
+        Spacer()
+        
+        LogoView()
+          .opacity(isAnimated ? 1 : 0)
+          .offset(x: 0, y: isAnimated ? 0 : -25)
+          .onAppear(perform: {
+            withAnimation(.easeOut(duration: 0.5)) {
+              isAnimated.toggle()
+            }
+          })
+        
+        Spacer()
+        
+        Button(action: {}, label: {
+          ZStack {
+            Image(systemName: "cart")
+              .font(.title)
+              .foregroundColor(.black)
+            
+            Circle()
+              .fill(Color.red)
+              .frame(width: 14, height: 14, alignment: .center)
+              .offset(x: 13, y: -10)
+          }
+        }) //: BUTTON
+      } //: HSTACK
     }
 }
 
