b2e3a870cdf87e532448a143492446079c5e3b10
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:37:21 +0300

    Some properties added

--- Touchdown/Touchdown/View/Home/ProductItemView.swift
+++ Touchdown/Touchdown/View/Home/ProductItemView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ProductItemView: View {
+    // MARK: - PROPERTY
+    
+    let product: Product
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
