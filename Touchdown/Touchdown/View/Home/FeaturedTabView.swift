c54b1df6769bf936971f9fd28da431096a764962
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-01 19:42:18 +0300

    FeaturedTab view UI configurations added

--- Touchdown/Touchdown/View/Home/FeaturedTabView.swift
+++ Touchdown/Touchdown/View/Home/FeaturedTabView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct FeaturedTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      TabView {
+        ForEach(players) { player in
+          FeaturedItemView(player: player)
+            .padding(.top, 10)
+            .padding(.horizontal, 15)
+        }
+      } //: TAB
+      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
     }
 }
 
