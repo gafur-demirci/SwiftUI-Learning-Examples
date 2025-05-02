b3c50094ad7c27147b67c7893269f5b9aa4bf127
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-02 22:18:59 +0300

    Some marks added

--- Devote/Devote/Model/Persistence.swift
+++ Devote/Devote/Model/Persistence.swift
@@ -8,29 +8,14 @@
 import CoreData
 
 struct PersistenceController {
+    
+    // MARK: - 1. PERSISTENT CONTROLLER
     static let shared = PersistenceController()
 
-    @MainActor
-    static let preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-
+    // MARK: - 2. PERSISTENT CONTAINER
     let container: NSPersistentContainer
-
+    
+    // MARK: - 3. INITIALIZATION (load the persistent store)
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "Devote")
         if inMemory {
