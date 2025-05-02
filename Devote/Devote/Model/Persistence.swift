af9a238588902a99053fb413caab9040558364d6
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-02 22:19:15 +0300

    sample preview added

--- Devote/Devote/Model/Persistence.swift
+++ Devote/Devote/Model/Persistence.swift
@@ -39,4 +39,27 @@ struct PersistenceController {
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
+    
+    // MARK: - 4. PREVIEW
+    @MainActor
+    static let preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        for i in 0..<5 {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+            newItem.task = "Preview \(i)"
+            newItem.completion = false
+            newItem.id = UUID()
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
 }
