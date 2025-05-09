af08ba95f69f4bc565e538698b0285cb17384703
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-09 20:52:20 +0300

    Refactor FooterView to implement booking button functionality and enhance layout

--- Honeymoon/Honeymoon/View/FooterView.swift
+++ Honeymoon/Honeymoon/View/FooterView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct FooterView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @Binding var showBookingAlert: Bool
+    let haptics = UINotificationFeedbackGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            
+            Spacer()
+            
+            Button(action: {
+                print("Success!")
+                playSound(sound: "sound-click", type: "mp3")
+                self.haptics.notificationOccurred(.success)
+                self.showBookingAlert.toggle()
+            }, label: {
+                Text("Book Destination".uppercased())
+                    .font(.system(.subheadline, design: .rounded))
+                    .fontWeight(.heavy)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .tint(Color.pink)
+                    .background(
+                        Capsule()
+                            .stroke(.pink, lineWidth: 2)
+                    )
+            })
+            
+            Spacer()
+            
+            Image(systemName: "heart.fill")
+                .font(.system(size: 42, weight: .light))
+        } //: HSTACK
+        .padding()
     }
 }
 
 #Preview {
-    FooterView()
+    @Previewable @State var showAlert: Bool = false
+    FooterView(showBookingAlert: $showAlert)
 }
