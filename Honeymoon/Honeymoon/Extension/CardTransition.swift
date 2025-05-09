6a7773e0f93cf8b738f6f283e066aa00a7041b8e
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-09 20:51:36 +0300

    Add custom AnyTransition effects for trailing and leading bottom animations

--- Honeymoon/Honeymoon/Extension/CardTransition.swift
+++ Honeymoon/Honeymoon/Extension/CardTransition.swift
@@ -5,4 +5,14 @@
 //  Created by Abdulgafur Demirci on 6.05.2025.
 //
 
-import Foundation
+import SwiftUI
+
+extension AnyTransition {
+    static var trailingBottom: AnyTransition {
+        AnyTransition.asymmetric(insertion: .identity, removal: AnyTransition.move(edge: .trailing).combined(with: .move(edge: .bottom)))
+    }
+    
+    static var leadingBottom: AnyTransition {
+        AnyTransition.asymmetric(insertion: .identity, removal: AnyTransition.move(edge: .leading).combined(with: .move(edge: .bottom)))
+    }
+}
