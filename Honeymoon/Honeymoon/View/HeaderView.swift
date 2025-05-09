c53a112b944776fd2f4078d59492a6498d9a50af
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-09 20:52:29 +0300

    Refactor HeaderView to implement layout and enhance user interaction

--- Honeymoon/Honeymoon/View/HeaderView.swift
+++ Honeymoon/Honeymoon/View/HeaderView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct HeaderView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @Binding var showGuideView: Bool
+    @Binding var showInfoView: Bool
+    let haptics = UINotificationFeedbackGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button(action: {
+                playSound(sound: "sound-click", type: "mp3")
+                self.haptics.notificationOccurred(.success)
+                self.showInfoView.toggle()
+            }, label: {
+                Image(systemName: "info.circle")
+                    .font(.system(size: 24, weight: .regular))
+            })
+            .tint(Color.primary)
+            
+            Spacer()
+            
+            Image("logo-honeymoon-pink")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 28)
+            
+            Spacer()
+            
+            Button(action: {
+                playSound(sound: "sound-click", type: "mp3")
+                self.haptics.notificationOccurred(.success)
+                self.showGuideView.toggle()
+            }, label: {
+                Image(systemName: "questionmark.circle")
+                    .font(.system(size: 24, weight: .regular))
+            })
+            .tint(Color.primary)
+            .sheet(isPresented: $showGuideView, content: {
+                GuideView()
+            })
+        } //: HSTACK
+        .padding()
     }
 }
 
 #Preview {
-    HeaderView()
+    @Previewable @State var showGuide: Bool = false
+    @Previewable @State var showInfo: Bool = false
+    HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
 }
