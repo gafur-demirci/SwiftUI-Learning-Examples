575011363c94cfacf910b20827a27faebfb3570a
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-09 20:51:19 +0300

    Implement GuideComponent with title, subtitle, description, and icon properties

--- Honeymoon/Honeymoon/Component/GuideComponent.swift
+++ Honeymoon/Honeymoon/Component/GuideComponent.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct GuideComponent: View {
+    
+    // MARK: - PROPERTIES
+    var title: String
+    var subtitle: String
+    var description: String
+    var icon: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center, spacing: 20 ) {
+            Image(systemName: icon)
+                .font(.largeTitle)
+                .foregroundColor(.pink)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                HStack {
+                    Text(title.uppercased())
+                        .font(.title)
+                        .fontWeight(.heavy)
+                    
+                    Spacer()
+                    
+                    Text(subtitle.uppercased())
+                        .font(.footnote)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.pink)
+                    
+                }
+                Divider()
+                    .padding(.bottom, 4)
+                Text(description)
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+        }
     }
 }
 
 #Preview {
-    GuideComponent()
+    GuideComponent(title: "Title", subtitle: "Swipe Right", description: "This is a placeholder sentence. This is a placeholder sentence. This is a placeholder sentence.", icon: "heart.circle")
 }
