1659e0c6bbb70f68198a1f7485a7a08ba61c23db
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-09 20:52:25 +0300

    Refactor GuideView to implement layout and enhance user interaction

--- Honeymoon/Honeymoon/View/GuideView.swift
+++ Honeymoon/Honeymoon/View/GuideView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct GuideView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                HeaderComponent()
+
+                Spacer()
+
+                Text("Get Started!")
+                    .fontWeight(.black)
+                    .modifier(TitleModifier())
+
+                Text("Discover and pick the perfect destination for your romantic honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+
+                Spacer(minLength: 10)
+
+                VStack(alignment: .leading, spacing: 25) {
+                    GuideComponent(title: "Like", subtitle: "Swipe right", description: "Do you like this destination? Touch the screen and swipe right. It will be saved to the favourites.", icon: "heart.circle")
+                    
+                    GuideComponent(title: "Dismiss", subtitle: "Swipe left", description: "Would your rather skip this place? Touch the screen and swipe left. You will no longer see it.", icon: "xmark.circle")
+                    
+                    GuideComponent(title: "Book", subtitle: "Tap the button", description: "Our selection of honeymoon resorts is perfect setting for you to embark your new life together.", icon: "checkmark.square")
+                }
+
+                Spacer(minLength: 10)
+
+                Button(action: {
+                    print("A button was tapped.")
+                    self.presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Text("Continue")
+                        .modifier(ButtonModifier())
+                })
+ 
+            } //: VSTACK
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 15)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        } //: SCROLL
     }
 }
 
