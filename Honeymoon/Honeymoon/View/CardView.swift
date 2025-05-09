314cd1d88308bac13660c7f22837e5a45e9c3e66
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-09 20:52:11 +0300

    Refactor CardView to conform to Identifiable protocol and implement detailed view layout

--- Honeymoon/Honeymoon/View/CardView.swift
+++ Honeymoon/Honeymoon/View/CardView.swift
@@ -7,12 +7,53 @@
 
 import SwiftUI
 
-struct CardView: View {
+struct CardView: View, Identifiable {
+    
+    // MARK: - PROPERTIES
+    
+    let id = UUID()
+    var honeymoon: Destination
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(honeymoon.image)
+            .resizable()
+            .clipShape(RoundedRectangle(cornerRadius: 24))
+            .scaledToFit()
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .overlay(
+                VStack(alignment: .center, spacing: 12) {
+                    Text(honeymoon.place.uppercased())
+                        .foregroundStyle(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .shadow(radius: 1)
+                        .padding(.horizontal, 18)
+                        .padding(.vertical, 4)
+                        .overlay(
+                            Rectangle()
+                                .fill(Color.white)
+                                .frame(height: 1),
+                            alignment: .bottom
+                        )
+                    Text(honeymoon.country.uppercased())
+                        .foregroundStyle(.black)
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .frame(minWidth: 85)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 5)
+                        .background(
+                            Capsule()
+                                .fill(.white)
+                        )
+                }
+                    .frame(minWidth: 280)
+                    .padding(.bottom, 50),
+                alignment: .bottom
+            )
     }
 }
 
 #Preview {
-    CardView()
+    CardView(honeymoon: HoneymoonData[1])
 }
